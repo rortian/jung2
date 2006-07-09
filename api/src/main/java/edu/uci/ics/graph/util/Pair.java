dc776216000d2762232b06ed6ea173a72e4fa798
tomnelson <tomnelson>
2006-07-09 13:50:13 +0000

    changed to implement Collection and delegate to anunmodifiable ArrayList

--- api/src/main/java/edu/uci/ics/graph/util/Pair.java
+++ api/src/main/java/edu/uci/ics/graph/util/Pair.java
@@ -11,6 +11,13 @@
  */
 package edu.uci.ics.graph.util;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 
 /**
 * Stores a pair of values together. Access either one by directly
@@ -23,17 +30,17 @@ package edu.uci.ics.graph.util;
 * @author scott white and Danyel Fisher
 */
 
-public final class Pair<T>
+public final class Pair<T> implements Collection<T> 
 {
-    private final T value1;
-    private final T value2;
+    private List<T> list = new ArrayList<T>(2);
+    private final List<T> delegate;
 
     public Pair(T value1, T value2) 
     {
-        if ( value1 == null  || value2 == null)
-            throw new IllegalArgumentException("Both inputs must be non-null");
-        this.value1 = value1;
-        this.value2 = value2;
+        assert value1 != null && value2 != null : "Both inputs must be non-null";
+        list.add(value1);
+        list.add(value2);
+        delegate = Collections.unmodifiableList(list);
     }
 
     /**
@@ -41,7 +48,7 @@ public final class Pair<T>
      */
     public T getFirst() 
     {
-        return value1;
+        return delegate.get(0);
     }
     
     /**
@@ -49,22 +56,27 @@ public final class Pair<T>
      */
     public T getSecond() 
     {
-        return value2;
+        return delegate.get(1);
     }
     
     /**
      * @see java.lang.Object#equals(java.lang.Object)
      */
     public boolean equals( Object o ) {
-        if (o instanceof Pair) 
-        {
-            Pair tt = (Pair) o;
-            Object first = tt.getFirst();
-            Object second = tt.getSecond();
-            return ((first == value1 || first.equals(value1)) &&
-                    (second == value2 || second.equals(value2)));
+        if (o == this)
+            return true;
+        if (!(o instanceof Pair))
+            return false;
+
+        Iterator<T> e1 = delegate.iterator();
+        Iterator e2 = ((Pair) o).iterator();
+        while(e1.hasNext() && e2.hasNext()) {
+            T o1 = e1.next();
+            Object o2 = e2.next();
+            if (!(o1==null ? o2==null : o1.equals(o2)))
+                return false;
         }
-        return false;
+        return !(e1.hasNext() || e2.hasNext());
     }
     
     /**
@@ -72,7 +84,7 @@ public final class Pair<T>
      */
     public int hashCode() 
     {
-        return value1.hashCode() + value2.hashCode();
+        return delegate.hashCode();
     }
     
     /**
@@ -80,7 +92,59 @@ public final class Pair<T>
      */
     public String toString()
     {
-        return "<" + value1.toString() + ", " + value2.toString() + ">";
+        return delegate.toString();
+    }
+
+    public boolean add(T o) {
+        return delegate.add(o);
+    }
+
+    public boolean addAll(Collection<? extends T> c) {
+        return delegate.addAll(c);
+    }
+
+    public void clear() {
+        delegate.clear();
+    }
+
+    public boolean contains(Object o) {
+        return delegate.contains(o);
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        return delegate.containsAll(c);
+    }
+
+    public boolean isEmpty() {
+        return delegate.isEmpty();
+    }
+
+    public Iterator<T> iterator() {
+        return delegate.iterator();
+    }
+
+    public boolean remove(Object o) {
+        return delegate.remove(o);
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        return delegate.removeAll(c);
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        return delegate.retainAll(c);
+    }
+
+    public int size() {
+        return delegate.size();
+    }
+
+    public Object[] toArray() {
+        return delegate.toArray();
+    }
+
+    public <S> S[] toArray(S[] a) {
+        return delegate.<S>toArray(a);
     }
 }
 
