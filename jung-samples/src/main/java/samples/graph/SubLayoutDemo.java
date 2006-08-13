b3350cac4ab72ada9be429d507304cf01ab99f51
tomnelson <tomnelson>
2006-08-13 19:40:28 +0000

    renamed 'samples' to 'jung-samples for betterconsistency with build artifact name

---
+++ jung-samples/src/main/java/samples/graph/SubLayoutDemo.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright (c) 2003, the JUNG Project and the Regents of the University of
+ * California All rights reserved.
+ * 
+ * This software is open-source under the BSD license; see either "license.txt"
+ * or http://jung.sourceforge.net/license.txt for a description.
+ * 
+ */
+package samples.graph;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Point2D;
+
+import javax.swing.BorderFactory;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import edu.uci.ics.graph.Graph;
+import edu.uci.ics.jung.graph.TestGraphs;
+import edu.uci.ics.jung.visualization.DefaultVisualizationModel;
+import edu.uci.ics.jung.visualization.GraphZoomScrollPane;
+import edu.uci.ics.jung.visualization.VisualizationModel;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.control.CrossoverScalingControl;
+import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.ScalingControl;
+import edu.uci.ics.jung.visualization.decorators.DefaultToolTipFunction;
+import edu.uci.ics.jung.visualization.decorators.PickableEdgePaintFunction;
+import edu.uci.ics.jung.visualization.decorators.PickableVertexPaintFunction;
+import edu.uci.ics.jung.visualization.layout.FRLayout;
+import edu.uci.ics.jung.visualization.picking.MultiPickedState;
+import edu.uci.ics.jung.visualization.picking.PickedState;
+import edu.uci.ics.jung.visualization.subLayout.CircularSubLayout;
+import edu.uci.ics.jung.visualization.subLayout.SubLayout;
+import edu.uci.ics.jung.visualization.subLayout.SubLayoutDecorator;
+
+/**
+ * Demonstrates the Cluster, CircularCluster, and ClusteringLayout
+ * classes. In this demo, vertices are visually clustered as they
+ * are selected. The cluster is formed in a circle centered at the
+ * location of the first vertex selected.
+ * 
+ * @author Tom Nelson - RABA Technologies
+ * 
+ */
+public class SubLayoutDemo extends JApplet {
+
+    String instructions =
+        "<html>Use the mouse to select multiple vertices"+
+        "<p>either by dragging a region, or by shift-clicking"+
+        "<p>on multiple vertices."+
+        "<p>As you select vertices, they become part of a"+
+        "<p>cluster, centered at the location of the first"+
+        "<p>selected vertex." +
+        "<p>You can drag the cluster with the mouse." +
+        "<p>Use the 'Picking'/'Transforming' combo-box to switch"+
+        "<p>between picking and transforming mode.</html>";
+    /**
+     * the graph
+     */
+    Graph<String,Number> graph;
+
+    /**
+     * the visual component and renderer for the graph
+     */
+    VisualizationViewer<String,Number> vv;
+
+    SubLayoutDecorator<String,Number> clusteringLayout;
+    
+    PickedState<String> ps;
+    
+    /**
+     * create an instance of a simple graph with controls to
+     * demo the zoomand hyperbolic features.
+     * 
+     */
+    public SubLayoutDemo() {
+        
+        // create a simple graph for the demo
+        graph = TestGraphs.getOneComponentGraph();
+
+        // ClusteringLayout is a decorator class that delegates
+        // to another layout, but can also sepately manage the
+        // layout of sub-sets of vertices in circular clusters.
+        clusteringLayout = new SubLayoutDecorator<String,Number>(new FRLayout<String,Number>(graph));
+
+        Dimension preferredSize = new Dimension(400,400);
+        final VisualizationModel<String,Number> visualizationModel = 
+            new DefaultVisualizationModel<String,Number>(clusteringLayout, preferredSize);
+        vv =  new VisualizationViewer<String,Number>(visualizationModel, preferredSize);
+        
+        vv.setPickedVertexState(new ClusterListener<String,Number>(clusteringLayout));
+        ps = vv.getPickedVertexState();
+        vv.getRenderContext().setEdgePaintFunction(new PickableEdgePaintFunction<String,Number>(vv.getPickedEdgeState(), Color.black, Color.red));
+        vv.getRenderContext().setVertexPaintFunction(new PickableVertexPaintFunction<String>(vv.getPickedVertexState(), 
+                Color.black, Color.red, Color.yellow));
+        vv.setBackground(Color.white);
+        
+        // add a listener for ToolTips
+        vv.setToolTipFunction(new DefaultToolTipFunction());
+        
+        /**
+         * the regular graph mouse for the normal view
+         */
+        final DefaultModalGraphMouse graphMouse = new DefaultModalGraphMouse();
+
+        vv.setGraphMouse(graphMouse);
+        
+        Container content = getContentPane();
+        GraphZoomScrollPane gzsp = new GraphZoomScrollPane(vv);
+        content.add(gzsp);
+        
+        JComboBox modeBox = graphMouse.getModeComboBox();
+        modeBox.addItemListener(graphMouse.getModeListener());
+        graphMouse.setMode(ModalGraphMouse.Mode.PICKING);
+        
+        final ScalingControl scaler = new CrossoverScalingControl();
+
+        JButton plus = new JButton("+");
+        plus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                scaler.scale(vv, 1.1f, vv.getCenter());
+            }
+        });
+        JButton minus = new JButton("-");
+        minus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                scaler.scale(vv, 1/1.1f, vv.getCenter());
+            }
+        });
+        
+        JButton help = new JButton("Help");
+        help.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog((JComponent)e.getSource(), instructions, "Help", JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+
+        JPanel controls = new JPanel();
+        JPanel zoomControls = new JPanel(new GridLayout(2,1));
+        zoomControls.setBorder(BorderFactory.createTitledBorder("Zoom"));
+        zoomControls.add(plus);
+        zoomControls.add(minus);
+        controls.add(zoomControls);
+        controls.add(modeBox);
+        controls.add(help);
+        content.add(controls, BorderLayout.SOUTH);
+    }
+    
+    class ClusterListener<V,E> extends MultiPickedState<V> {
+
+        SubLayoutDecorator<V,E> layout;
+        Point2D center;
+        /* (non-Javadoc)
+         * @see edu.uci.ics.jung.visualization.MultiPickedState#pick(edu.uci.ics.jung.graph.ArchetypeVertex, boolean)
+         */
+        public boolean pick(V v, boolean picked) {
+            boolean result = super.pick(v, picked);
+            if(picked) {
+                vertexPicked(v);
+            } else {
+                vertexUnpicked(v);
+            }
+            return result;
+        }
+        public ClusterListener(SubLayoutDecorator<V,E> layout) {
+            this.layout = layout;
+        }
+        public void vertexPicked(V v) {
+            if(center == null) {
+                center = layout.getLocation(v);
+            }
+            layout.removeAllSubLayouts();
+            SubLayout<V> subLayout = new CircularSubLayout<V>(getPicked(), 20, center);
+            layout.addSubLayout(subLayout);
+        }
+
+        public void vertexUnpicked(V v) {
+            layout.removeAllSubLayouts();
+            if(this.getPicked().isEmpty() == false) {
+                SubLayout<V> subLayout = new CircularSubLayout<V>(getPicked(), 20, center);
+                layout.addSubLayout(subLayout);
+            } else {
+                center = null;
+            }
+        }
+    }
+
+    /**
+     * a driver for this demo
+     */
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().add(new SubLayoutDemo());
+        f.pack();
+        f.setVisible(true);
+    }
+}
---
+++ jung-samples/src/main/java/samples/graph/VertexCollapseDemo.java
@@ -0,0 +1,262 @@
+/*
+ * Copyright (c) 2003, the JUNG Project and the Regents of the University of
+ * California All rights reserved.
+ * 
+ * This software is open-source under the BSD license; see either "license.txt"
+ * or http://jung.sourceforge.net/license.txt for a description.
+ * 
+ */
+package samples.graph;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Shape;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Point2D;
+import java.util.Collection;
+import java.util.HashSet;
+
+import javax.swing.BorderFactory;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import edu.uci.ics.graph.Graph;
+import edu.uci.ics.jung.graph.TestGraphs;
+import edu.uci.ics.jung.visualization.DefaultVisualizationModel;
+import edu.uci.ics.jung.visualization.GraphZoomScrollPane;
+import edu.uci.ics.jung.visualization.VisualizationModel;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.control.CrossoverScalingControl;
+import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.ScalingControl;
+import edu.uci.ics.jung.visualization.decorators.ConstantVertexSizeFunction;
+import edu.uci.ics.jung.visualization.decorators.DefaultToolTipFunction;
+import edu.uci.ics.jung.visualization.decorators.EllipseVertexShapeFunction;
+import edu.uci.ics.jung.visualization.layout.FRLayout;
+import edu.uci.ics.jung.visualization.layout.Layout;
+import edu.uci.ics.jung.visualization.subLayout.GraphCollapser;
+
+
+/**
+ * 
+ * @author Tom Nelson - RABA Technologies
+ * 
+ */
+public class VertexCollapseDemo extends JApplet {
+
+    String instructions =
+        "<html>Use the mouse to select multiple vertices"+
+        "<p>either by dragging a region, or by shift-clicking"+
+        "<p>on multiple vertices."+
+        "<p>After you select vertices, use the Collapse button"+
+        "<p>to combine them into a single vertex."+
+        "<p>Select a 'collapsed' vertex and use the Expand button"+
+        "<p>to restore the collapsed vertices."+
+        "<p>The Restore button will restore the original graph."+
+        "<p>You can drag the vertices with the mouse." +
+        "<p>Use the 'Picking'/'Transforming' combo-box to switch"+
+        "<p>between picking and transforming mode.</html>";
+    /**
+     * the graph
+     */
+    Graph graph;
+
+    /**
+     * the visual component and renderer for the graph
+     */
+    VisualizationViewer vv;
+    
+    Layout layout;
+    
+    GraphCollapser collapser;
+    /**
+     * create an instance of a simple graph with controls to
+     * demo the zoomand hyperbolic features.
+     * 
+     */
+    public VertexCollapseDemo() {
+        
+        // create a simple graph for the demo
+        graph = 
+            TestGraphs.getOneComponentGraph();
+        collapser = new GraphCollapser(graph);
+        
+        layout = new FRLayout(graph);
+
+        Dimension preferredSize = new Dimension(400,400);
+        final VisualizationModel visualizationModel = 
+            new DefaultVisualizationModel(layout, preferredSize);
+        vv =  new VisualizationViewer(visualizationModel, preferredSize);
+        
+        vv.getRenderContext().setVertexShapeFunction(new ClusterVertexShapeFunction());
+
+        vv.setBackground(Color.white);
+        
+        // add a listener for ToolTips
+        vv.setToolTipFunction(new DefaultToolTipFunction());
+        
+        /**
+         * the regular graph mouse for the normal view
+         */
+        final DefaultModalGraphMouse graphMouse = new DefaultModalGraphMouse();
+
+        vv.setGraphMouse(graphMouse);
+        
+        Container content = getContentPane();
+        GraphZoomScrollPane gzsp = new GraphZoomScrollPane(vv);
+        content.add(gzsp);
+        
+        JComboBox modeBox = graphMouse.getModeComboBox();
+        modeBox.addItemListener(graphMouse.getModeListener());
+        graphMouse.setMode(ModalGraphMouse.Mode.PICKING);
+        
+        final ScalingControl scaler = new CrossoverScalingControl();
+
+        JButton plus = new JButton("+");
+        plus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                scaler.scale(vv, 1.1f, vv.getCenter());
+            }
+        });
+        JButton minus = new JButton("-");
+        minus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                scaler.scale(vv, 1/1.1f, vv.getCenter());
+            }
+        });
+        
+        JButton collapse = new JButton("Collapse");
+        collapse.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                Collection picked = new HashSet(vv.getPickedVertexState().getPicked());
+                if(picked.size() > 1) {
+                    Graph inGraph = layout.getGraph();
+                    Graph clusterGraph = collapser.getClusterGraph(inGraph, picked);
+
+                    Graph g = collapser.collapse(layout.getGraph(), clusterGraph);
+                    double sumx = 0;
+                    double sumy = 0;
+                    for(Object v : picked) {
+                    	Point2D p = layout.getLocation(v);
+                    	sumx += p.getX();
+                    	sumy += p.getY();
+                    }
+                    Point2D cp = new Point2D.Double(sumx/picked.size(), sumy/picked.size());
+                    vv.stop();
+                    vv.getRenderContext().getParallelEdgeIndexFunction().reset();
+                    layout.setGraph(g);
+                    layout.forceMove(clusterGraph, cp.getX(), cp.getY());
+                }
+            }});
+        
+        JButton expand = new JButton("Expand");
+        expand.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                Collection picked = vv.getPickedVertexState().getPicked();
+                for(Object v : picked) {
+                    if(v instanceof Graph) {
+                        
+                        Graph g = collapser.expand(layout.getGraph(), (Graph)v);
+                        
+                        vv.stop();
+                        vv.getRenderContext().getParallelEdgeIndexFunction().reset();
+                        layout.setGraph(g);
+                    }
+                    vv.repaint();
+                }
+            }});
+        
+        JButton reset = new JButton("Reset");
+        reset.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                vv.stop();
+                layout.setGraph(graph);
+                vv.restart();
+            }});
+        
+        JButton help = new JButton("Help");
+        help.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog((JComponent)e.getSource(), instructions, "Help", JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+
+        JPanel controls = new JPanel();
+        JPanel zoomControls = new JPanel(new GridLayout(2,1));
+        zoomControls.setBorder(BorderFactory.createTitledBorder("Zoom"));
+        zoomControls.add(plus);
+        zoomControls.add(minus);
+        controls.add(zoomControls);
+        JPanel collapseControls = new JPanel(new GridLayout(3,1));
+        collapseControls.setBorder(BorderFactory.createTitledBorder("Picked"));
+        collapseControls.add(collapse);
+        collapseControls.add(expand);
+        collapseControls.add(reset);
+        controls.add(collapseControls);
+        controls.add(modeBox);
+        controls.add(help);
+        content.add(controls, BorderLayout.SOUTH);
+    }
+    
+    class ClusterVertexShapeFunction<V> extends EllipseVertexShapeFunction<V> {
+
+        ClusterVertexShapeFunction() {
+            setSizeFunction(new ClusterVertexSizeFunction<V>(20));
+        }
+        @Override
+        public Shape getShape(V v) {
+            if(v instanceof Graph) {
+                int size = ((Graph)v).getVertices().size();
+                if (size < 8) {   
+                    int sides = Math.max(size, 3);
+                    return factory.getRegularPolygon(v, sides);
+                }
+                else {
+                    return factory.getRegularStar(v, size);
+                }
+            }
+            return super.getShape(v);
+        }
+    }
+    
+    class ClusterVertexSizeFunction<V> extends ConstantVertexSizeFunction<V> {
+
+        public ClusterVertexSizeFunction(int size) {
+            super(size);
+        }
+
+        @Override
+        public int getSize(V v) {
+            if(v instanceof Graph) {
+                return 30;
+            }
+            return super.getSize(v);
+        }
+    }
+
+    /**
+     * a driver for this demo
+     */
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().add(new VertexCollapseDemo());
+        f.pack();
+        f.setVisible(true);
+    }
+}
+
+
