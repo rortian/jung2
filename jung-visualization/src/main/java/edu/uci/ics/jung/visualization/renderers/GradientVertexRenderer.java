975d4c5e7c8986beaf36452f27f13d779870aa7f
tomnelson <tomnelson>
2006-12-06 15:22:19 +0000

    first commit

---
+++ jung-visualization/src/main/java/edu/uci/ics/jung/visualization/renderers/GradientVertexRenderer.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2005, the JUNG Project and the Regents of the University of
+ * California All rights reserved.
+ *
+ * This software is open-source under the BSD license; see either "license.txt"
+ * or http://jung.sourceforge.net/license.txt for a description.
+ *
+ * Created on Aug 23, 2005
+ */
+package edu.uci.ics.jung.visualization.renderers;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GradientPaint;
+import java.awt.Paint;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.Stroke;
+import java.awt.geom.AffineTransform;
+
+import javax.swing.JComponent;
+
+import edu.uci.ics.graph.Graph;
+import edu.uci.ics.jung.visualization.RenderContext;
+import edu.uci.ics.jung.visualization.Renderer;
+import edu.uci.ics.jung.visualization.transform.shape.GraphicsDecorator;
+
+/**
+ * A renderer that will fill vertex shapes with a GradientPaint
+ * @author Tom Nelson
+ *
+ * @param <V>
+ * @param <E>
+ */
+public class GradientVertexRenderer<V,E> implements Renderer.Vertex<V,E> {
+	
+	Color colorOne;
+	Color colorTwo;
+	boolean cyclic;
+	
+
+    public GradientVertexRenderer(Color colorOne, Color colorTwo, boolean cyclic) {
+		this.colorOne = colorOne;
+		this.colorTwo = colorTwo;
+		this.cyclic = cyclic;
+	}
+
+	public void paintVertex(RenderContext<V,E> rc, Graph<V,E> graph, V v, int x, int y) {
+        if (rc.getVertexIncludePredicate().evaluateVertex(graph, v)) {
+            boolean vertexHit = true;
+            // get the shape to be rendered
+            Shape shape = rc.getVertexShapeFunction().transform(v);
+            
+            // create a transform that translates to the location of
+            // the vertex to be rendered
+            AffineTransform xform = AffineTransform.getTranslateInstance(x,y);
+            // transform the vertex shape with xtransform
+            shape = xform.createTransformedShape(shape);
+            
+            vertexHit = vertexHit(rc, shape);
+                //rc.getViewTransformer().transform(shape).intersects(deviceRectangle);
+
+            if (vertexHit) {
+            	paintShapeForVertex(rc, v, shape);
+            }
+        }
+    }
+    
+    protected boolean vertexHit(RenderContext<V,E> rc, Shape s) {
+        JComponent vv = rc.getScreenDevice();
+        Rectangle deviceRectangle = null;
+        if(vv != null) {
+//            deviceRectangle = vv.getBounds();
+            Dimension d = vv.getSize();
+            if(d.width <= 0 || d.height <= 0) {
+                d = vv.getPreferredSize();
+            }
+            deviceRectangle = new Rectangle(
+                    0,0,
+                    d.width,d.height);
+        }
+        return rc.getViewTransformer().transform(s).intersects(deviceRectangle);
+    }
+
+    protected void paintShapeForVertex(RenderContext<V,E> rc, V v, Shape shape) {
+        GraphicsDecorator g = rc.getGraphicsContext();
+        Paint oldPaint = g.getPaint();
+        Rectangle r = shape.getBounds();
+        Paint fillPaint = new GradientPaint((float)r.getMinX(), (float)r.getMinY(), colorOne,
+        		(float)r.getMinX(), (float)(r.getMinY()+r.getHeight()/2), colorTwo, true);
+        if(fillPaint != null) {
+            g.setPaint(fillPaint);
+            g.fill(shape);
+            g.setPaint(oldPaint);
+        }
+        Paint drawPaint = rc.getVertexDrawPaintFunction().transform(v);
+        if(drawPaint != null) {
+            g.setPaint(drawPaint);
+        }
+        Stroke oldStroke = g.getStroke();
+        Stroke stroke = rc.getVertexStrokeFunction().transform(v);
+        if(stroke != null) {
+            g.setStroke(stroke);
+        }
+        g.draw(shape);
+        g.setPaint(oldPaint);
+        g.setStroke(oldStroke);
+    }
+}
