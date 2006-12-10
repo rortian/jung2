b9fc22421b1efba744548eba5a7e15b02d70ea87
tomnelson <tomnelson>
2006-12-10 01:05:41 +0000

    first commit

---
+++ jung-algorithms/src/main/java/edu/uci/ics/jung/algorithms/importance/HITS.java
@@ -0,0 +1,219 @@
+/*
+* Copyright (c) 2003, the JUNG Project and the Regents of the University
+* of California
+* All rights reserved.
+*
+* This software is open-source under the BSD license; see either
+* "license.txt" or
+* http://jung.sourceforge.net/license.txt for a description.
+*/
+package edu.uci.ics.jung.algorithms.importance;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.uci.ics.graph.Graph;
+
+
+/**
+ * Calculates the "hubs-and-authorities" importance measures for each node in a graph.
+ * These measures are defined recursively as follows:
+ *
+ * <ul>
+ * <li>The *hubness* of a node is the degree to which a node links to other important authorities</li>
+ * <li>The *authoritativeness* of a node is the degree to which a node is pointed to by important hubs</li>
+ * <p>
+ * Note: This algorithm uses the same key as HITSWithPriors for storing rank sccores.
+ * <p>
+ * A simple example of usage is:
+ * <pre>
+ * HITS ranker = new HITS(someGraph);
+ * ranker.evaluate();
+ * ranker.printRankings();
+ * </pre>
+ * <p>
+ * Running time: O(|V|*I) where |V| is the number of vertices and I is the number of iterations until convergence
+ *
+ * @author Scott White
+ * @author Tom Nelson - adapted to jung2
+ * @see "Authoritative sources in a hyperlinked environment by Jon Kleinberg, 1997"
+ */
+public class HITS<V,E> extends AbstractRanker<V,E> {
+    protected static final String AUTHORITY_KEY = "jung.algorithms.importance.AUTHORITY";
+    protected static final String HUB_KEY = "jung.algorithms.importance.HUB";
+    private Object mKeyToUseForRanking;
+    private Map<V,Number> mPreviousAuthorityScores;
+    private Map<V,Number> mPreviousHubScores;
+
+    /**
+     * Constructs an instance of the ranker where the type of importance that is associated with the
+     * rank score is the node's importance as an authority.
+     * @param graph the graph whose nodes are to be ranked
+     * @param useAuthorityForRanking
+     */
+    public HITS(Graph<V,E> graph, boolean useAuthorityForRanking) {
+        mKeyToUseForRanking = AUTHORITY_KEY;
+        if (!useAuthorityForRanking) {
+        	mKeyToUseForRanking = HUB_KEY;
+        }
+        initialize(graph);
+    }
+
+    /**
+     * Constructs an instance of the ranker where the type of importance that is associated with the
+     * rank score is the node's importance as an authority.
+     * @param graph the graph whose nodes are to be ranked
+     */
+    public HITS(Graph<V,E> graph) {
+        mKeyToUseForRanking = AUTHORITY_KEY;
+        initialize(graph);
+    }
+
+    protected void initialize(Graph<V,E> g) {
+
+        super.initialize(g, true, false);
+
+        mPreviousAuthorityScores = new HashMap<V,Number>();
+        mPreviousHubScores = new HashMap<V,Number>();
+
+        for (V currentVertex : g.getVertices()) {
+            setVertexRankScore(currentVertex, 1.0, AUTHORITY_KEY);
+            setVertexRankScore(currentVertex, 1.0, HUB_KEY);
+
+            mPreviousAuthorityScores.put(currentVertex, 0);
+            mPreviousHubScores.put(currentVertex, 0);
+        }
+    }
+
+    protected void finalizeIterations() {
+        super.finalizeIterations();
+        for (V v : getVertices()) {
+            if (mKeyToUseForRanking.equals(AUTHORITY_KEY)) {
+            	super.removeVertexRankScore(v, HUB_KEY);
+            } else {
+            	super.removeVertexRankScore(v, AUTHORITY_KEY);
+            }
+        }
+    }
+
+    /**
+     * the user datum key used to store the rank scores
+     * @return the key
+     */
+    public Object getRankScoreKey() {
+        return mKeyToUseForRanking;
+    }
+
+    /**
+     * Given a node, returns the corresponding rank score. This implementation of <code>getRankScore</code> assumes
+     * the decoration representing the rank score is of type <code>MutableDouble</code>.
+     * @return the rank score for this node
+     */
+    public double getVertexRankScore(V v) {
+        return getVertexRankScore(v, mKeyToUseForRanking);
+    }
+
+    protected double getPreviousAuthorityScore(V v) {
+        return mPreviousAuthorityScores.get(v).doubleValue();
+    }
+
+    protected double getPreviousHubScore(V v) {
+        return mPreviousHubScores.get(v).doubleValue();
+    }
+
+    public void step() {
+        updatePreviousScores();
+
+        //Perform 2 update steps
+        updateAuthorityRankings();
+        updateHubRankings();
+
+        double hubMSE = 0;
+        double authorityMSE = 0;
+
+        //Normalize rankings and test for convergence
+        int numVertices = getVertices().size();
+        for (V v : getVertices()) {
+
+            double currentAuthorityScore = getVertexRankScore(v, AUTHORITY_KEY);
+            double currentHubScore = getVertexRankScore(v, HUB_KEY);
+
+            double previousAuthorityScore = getPreviousAuthorityScore(v);
+            double previousHubScore = getPreviousHubScore(v);
+
+            hubMSE += Math.pow(currentHubScore - previousHubScore, 2);
+            authorityMSE += Math.pow(currentAuthorityScore - previousAuthorityScore, 2);
+        }
+
+        hubMSE = Math.pow(hubMSE / numVertices, 0.5);
+        authorityMSE = Math.pow(authorityMSE / numVertices, 0.5);
+
+        setPrecision(hubMSE+authorityMSE);
+    }
+
+    /**
+     * If <code>evaluate()</code> has not already been called, the user can override the type of importance.
+     * (hub or authority) that should be associated with the rank score.
+     * @param useAuthorityForRanking if <code>true</code>, authority is used; if <code>false</code>, hub is used
+     */
+    public void setUseAuthorityForRanking(boolean useAuthorityForRanking) {
+        if (useAuthorityForRanking) {
+            mKeyToUseForRanking = AUTHORITY_KEY;
+        } else {
+            mKeyToUseForRanking = HUB_KEY;
+        }
+    }
+
+    private double computeSum(Collection<V> neighbors, Object key) {
+        double sum = 0;
+        for (V v : neighbors) {
+//            Vertex currentNeighbor = (Vertex) neighborIt.next();
+            sum += getVertexRankScore(v, key);
+        }
+        return sum;
+    }
+
+    private void normalizeRankings(double normConstant, Object key) {
+        for (V v : getVertices()) {
+            double rankScore = getVertexRankScore(v,key);
+            setVertexRankScore(v,rankScore/normConstant,key);
+        }
+    }
+
+    protected void updateAuthorityRankings() {
+        double total = 0;
+        //compute authority scores
+        for (V v : getVertices()) {
+            double currentHubSum = computeSum(getGraph().getPredecessors(v), HUB_KEY);
+            double newAuthorityScore = currentHubSum;
+            total += newAuthorityScore;
+            setVertexRankScore(v, newAuthorityScore, AUTHORITY_KEY);
+        }
+
+        normalizeRankings(total, AUTHORITY_KEY);
+    }
+
+    protected void updateHubRankings() {
+        double total = 0;
+
+        //compute hub scores
+        for (V v : getVertices()) {
+            double currentAuthoritySum = computeSum(getGraph().getSuccessors(v), AUTHORITY_KEY);
+            double newHubScore = currentAuthoritySum;
+            total += newHubScore;
+            setVertexRankScore(v, newHubScore, HUB_KEY);
+        }
+        normalizeRankings(total, HUB_KEY);
+    }
+
+    protected void updatePreviousScores() {
+        for (V v : getVertices()) {
+            double currentAuthorityScore = getVertexRankScore(v, AUTHORITY_KEY);
+            mPreviousAuthorityScores.put(v,currentAuthorityScore);
+
+            double currentHubScore = getVertexRankScore(v, HUB_KEY);
+            mPreviousHubScores.put(v, currentHubScore);
+        }
+    }
+}
---
+++ jung-algorithms/src/test/java/edu/uci/ics/jung/algorithms/importance/TestHITS.java
@@ -0,0 +1,73 @@
+/*
+* Copyright (c) 2003, the JUNG Project and the Regents of the University
+* of California
+* All rights reserved.
+*
+* This software is open-source under the BSD license; see either
+* "license.txt" or
+* http://jung.sourceforge.net/license.txt for a description.
+*/
+package edu.uci.ics.jung.algorithms.importance;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import edu.uci.ics.graph.DirectedGraph;
+import edu.uci.ics.jung.algorithms.importance.HITS;
+import edu.uci.ics.jung.graph.SimpleDirectedSparseGraph;
+
+
+/**
+ * @author Scott White
+ * @author Tom Nelson - adapted to jung2
+ */
+public class TestHITS extends TestCase {
+
+	DirectedGraph<Number,Number> graph;
+	
+    public static Test suite() {
+        return new TestSuite(TestHITS.class);
+    }
+
+    protected void setUp() {
+        graph = new SimpleDirectedSparseGraph<Number,Number>();
+        for(int i=0; i<5; i++) {
+        	graph.addVertex(i);
+        }
+
+        int j=0;
+        graph.addEdge(j++, 0, 1);
+        graph.addEdge(j++, 1, 2);
+        graph.addEdge(j++, 2, 3);
+        graph.addEdge(j++, 3, 0);
+        graph.addEdge(j++, 2, 1);
+    }
+
+    public void testRankerAuthorities() {
+
+        HITS<Number,Number> ranker = new HITS<Number,Number>(graph);
+        ranker.setRemoveRankScoresOnFinalize(false);
+        ranker.setMaximumIterations(500);
+        ranker.evaluate();
+
+        Assert.assertEquals(ranker.getVertexRankScore(0), 0, .0001);
+        Assert.assertEquals(ranker.getVertexRankScore(1), 0.618, .001);
+        Assert.assertEquals(ranker.getVertexRankScore(2), 0.0, .0001);
+        Assert.assertEquals(ranker.getVertexRankScore(3), 0.3819, .001);
+
+    }
+
+    public void testRankerHubs() {
+
+        HITS<Number,Number> ranker = new HITS<Number,Number>(graph,false);
+        ranker.setMaximumIterations(500);
+        ranker.setRemoveRankScoresOnFinalize(false);
+        ranker.evaluate();
+
+        Assert.assertEquals(ranker.getVertexRankScore(0), 0.38196, .001);
+        Assert.assertEquals(ranker.getVertexRankScore(1), 0.0, .0001);
+        Assert.assertEquals(ranker.getVertexRankScore(2), 0.618, .0001);
+        Assert.assertEquals(ranker.getVertexRankScore(3), 0.0, .0001);
+    }
+}
