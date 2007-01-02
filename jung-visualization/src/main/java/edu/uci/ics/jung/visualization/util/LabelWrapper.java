1835fb6fa112e6b1526e8dbc68657928c8646c02
tomnelson <tomnelson>
2007-01-02 17:50:04 +0000

    first commit

---
+++ jung-visualization/src/main/java/edu/uci/ics/jung/visualization/util/LabelWrapper.java
@@ -0,0 +1,74 @@
+package edu.uci.ics.jung.visualization.util;
+
+import org.apache.commons.collections15.Transformer;
+
+/**
+ * A utility to wrap long lines, creating html strings
+ * with 
+ * @author Tom Nelson - tomnelson@dev.java.net
+ *
+ */
+public class LabelWrapper implements Transformer<String,String> {
+
+	int lineLength;
+	public static final String breaker = "<p>";
+	
+	/**
+	 * Create an instance with default line break length = 10
+	 *
+	 */
+	public LabelWrapper() {
+		this(10);
+	}
+	
+	/**
+	 * Create an instance with passed line break length
+	 * @param lineLength the max length for lines
+	 */
+	public LabelWrapper(int lineLength) {
+		this.lineLength = lineLength;
+	}
+
+	/**
+	 * call 'wrap' to transform the passed String
+	 */
+	public String transform(String str) {
+		return wrap(str);
+	}
+	
+	/**
+	 * line-wrap the passed String as an html string with
+	 * break Strings inserted.
+	 * 
+	 * @param str
+	 * @return
+	 */
+	private String wrap(String str) {
+		StringBuilder buf = new StringBuilder(str);
+		int len = lineLength;
+		while(len < buf.length()) {
+			int idx = buf.lastIndexOf(" ", len);
+			if(idx != -1) {
+				buf.replace(idx, idx+1, breaker);
+				len = idx + breaker.length() +lineLength;
+			} else {
+				buf.insert(len, breaker);
+				len += breaker.length() + lineLength;
+			}
+		}
+		buf.insert(0, "<html>");
+		return buf.toString();
+	}
+	
+	public static void main(String[] args) {
+		String[] lines = {
+				"This is a line with many short words that I will break into shorter lines.",
+				"thisisalinewithnobreakssowhoknowswhereitwillwrap",
+				"short line"
+		};
+		LabelWrapper w = new LabelWrapper(10);
+		for(int i=0; i<lines.length; i++) {
+			System.err.println("from "+lines[i]+" to "+w.wrap(lines[i]));
+		}
+	}
+}
