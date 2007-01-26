b79627eb13302b36d0955dadb9bf2fd9d6c23cdd
tomnelson <tomnelson>
2007-01-26 19:44:26 +0000

    renamed to better describe

--- jung-graph-impl/src/main/java/edu/uci/ics/jung/graph/SimpleRootedDirectedSparseTree.java
+++ jung-graph-impl/src/main/java/edu/uci/ics/jung/graph/SimpleRootedDirectedSparseTree.java
@@ -2,6 +2,7 @@ package edu.uci.ics.jung.graph;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.collections15.Factory;
@@ -11,24 +12,43 @@ import edu.uci.ics.graph.Tree;
 import edu.uci.ics.graph.util.EdgeType;
 import edu.uci.ics.graph.util.Pair;
 
-public class SimpleSparseTree<V,E> implements Tree<V,E>, DirectedGraph<V,E> {
+/**
+ * An implementation of the Tree<V,E> interface that delegates to
+ * any implementation of DirectedGraph<V,E>
+ * @author Tom Nelson
+ *
+ * @param <V> the vertex type
+ * @param <E> the edge type
+ */
+public class SimpleRootedDirectedSparseTree<V,E> implements Tree<V,E>, DirectedGraph<V,E> {
 	
 	protected DirectedGraph<V,E> delegate;
 	protected Factory<E> edgeFactory;
 	protected V root;
 
-	public SimpleSparseTree(Factory<DirectedGraph<V,E>> graphFactory, 
+	/**
+	 * create an instance with passed values.
+	 * @param graphFactory must create a DirectedGraph to use as a delegate
+	 * @param edgeFactory must create unique edges to connect tree nodes
+	 */
+	public SimpleRootedDirectedSparseTree(Factory<DirectedGraph<V,E>> graphFactory, 
 			Factory<E> edgeFactory) {
 		this.delegate = graphFactory.create();
 		this.edgeFactory = edgeFactory;
 	}
 	
 	/**
-	 * @param e
-	 * @param v1
-	 * @param v2
-	 * @param edgeType
-	 * @return
+	 * Add an edge to the tree, connecting v1, the parent and v2, the child.
+	 * v1 must already exist in the tree, and v2 must not already exist
+	 * the passed edge must be unique in the tree. Passing an edgeType
+	 * other than EdgeType.DIRECTED may cause an illegal argument exception 
+	 * in the delegate graph.
+	 * 
+	 * @param e a unique edge to add
+	 * @param v1 the parent node
+	 * @param v2 the child node
+	 * @param edgeType should be EdgeType.DIRECTED
+	 * @return true if this call mutates the underlying graph
 	 * @see edu.uci.ics.graph.Graph#addEdge(java.lang.Object, java.lang.Object, java.lang.Object, edu.uci.ics.graph.util.EdgeType)
 	 */
 	public boolean addEdge(E e, V v1, V v2, EdgeType edgeType) {
@@ -36,10 +56,14 @@ public class SimpleSparseTree<V,E> implements Tree<V,E>, DirectedGraph<V,E> {
 	}
 
 	/**
-	 * @param e
-	 * @param v1
-	 * @param v2
-	 * @return
+	 * Add an edge to the tree, connecting v1, the parent and v2, the child.
+	 * v1 must already exist in the tree, and v2 must not already exist
+	 * the passed edge must be unique in the tree. 
+	 * 
+	 * @param e a unique edge to add
+	 * @param v1 the parent node
+	 * @param v2 the child node
+	 * @return true if this call mutates the underlying graph
 	 * @see edu.uci.ics.graph.Graph#addEdge(java.lang.Object, java.lang.Object, java.lang.Object)
 	 */
 	public boolean addEdge(E e, V v1, V v2) {
@@ -47,9 +71,13 @@ public class SimpleSparseTree<V,E> implements Tree<V,E>, DirectedGraph<V,E> {
 	}
 
 	/**
-	 * @param vertex
-	 * @return
+	 * Will set the root of the Tree, only if the Tree is empty and the
+	 * root is currently unset.
+	 * 
+	 * @param vertex the tree root to set
+	 * @return true if this call mutates the underlying graph
 	 * @see edu.uci.ics.graph.ArchetypeGraph#addVertex(java.lang.Object)
+	 * @throws an UnsupportedOperationException if the root was previously set
 	 */
 	public boolean addVertex(V vertex) {
 		if(root == null) {
@@ -345,6 +373,8 @@ public class SimpleSparseTree<V,E> implements Tree<V,E>, DirectedGraph<V,E> {
 	}
 
 	/**
+	 * remove the passed node, and all nodes that are descendants of the
+	 * passed node.
 	 * @param vertex
 	 * @return
 	 * @see edu.uci.ics.graph.ArchetypeGraph#removeVertex(java.lang.Object)
@@ -356,10 +386,29 @@ public class SimpleSparseTree<V,E> implements Tree<V,E>, DirectedGraph<V,E> {
 		return delegate.removeVertex(vertex);
 	}
 	
+	/**
+	 * add the passed child node as a child of parent.
+	 * parent must exist in the tree, and child must not already exist.
+	 * the connecting edge will be dynamically created by the 
+	 * edgeFactory member
+	 * @param parent the existing parent to attach the child to
+	 * @param child the new child to add to the tree as a child of parent
+	 * @return whether this call mutates the underlying graph
+	 */
 	public boolean addChild(V parent, V child) {
 		return addChild(edgeFactory.create(), parent, child);
 	}
 
+	/**
+	 * add the passed child node as a child of parent.
+	 * parent must exist in the tree, and child must not already exist.
+	 * 
+	 * @param edge the unique edge to connect the parent and child nodes
+	 * @param parent the existing parent to attach the child to
+	 * @param child the new child to add to the tree as a child of parent
+	 * @param edgeType must be EdgeType.DIRECTED or the underlying graph may throw an exception
+	 * @return whether this call mutates the underlying graph
+	 */
 	public boolean addChild(E edge, V parent, V child, EdgeType edgeType) {
 		Collection<V> vertices = delegate.getVertices();
 		if(vertices.contains(parent) == false) {
@@ -371,6 +420,14 @@ public class SimpleSparseTree<V,E> implements Tree<V,E>, DirectedGraph<V,E> {
 		return delegate.addEdge(edge, parent, child, edgeType);
 	}
 
+	/**
+	 * add the passed child node as a child of parent.
+	 * parent must exist in the tree, and child must not already exist
+	 * @param edge the unique edge to connect the parent and child nodes
+	 * @param parent the existing parent to attach the child to
+	 * @param child the new child to add to the tree as a child of parent
+	 * @return whether this call mutates the underlying graph
+	 */
 	public boolean addChild(E edge, V parent, V child) {
 		Collection<V> vertices = delegate.getVertices();
 		if(vertices.contains(parent) == false) {
@@ -381,14 +438,24 @@ public class SimpleSparseTree<V,E> implements Tree<V,E>, DirectedGraph<V,E> {
 		}
 		return delegate.addEdge(edge, parent, child);
 	}
+	
+	/**
+	 * get the number of children of the passed parent node
+	 */
 	public int getChildCount(V parent) {
-		return delegate.getSuccessors(parent).size();
+		return getChildren(parent).size();
 	}
 
+	/**
+	 * get the immediate children nodes of the passed parent
+	 */
 	public Collection<V> getChildren(V parent) {
 		return delegate.getSuccessors(parent);
 	}
 
+	/**
+	 * get the single parent node of the passed child
+	 */
 	public V getParent(V child) {
 		Collection<V> predecessors = delegate.getPredecessors(child);
 		if(predecessors.size() == 0) {
@@ -397,6 +464,13 @@ public class SimpleSparseTree<V,E> implements Tree<V,E>, DirectedGraph<V,E> {
 		return predecessors.iterator().next();
 	}
 
+	/**
+	 * returns an ordered list of the nodes beginning at the root
+	 * and ending at the passed child node, including all intermediate
+	 * nodes.
+	 * @param child the last node in the path from the root
+	 * @return an ordered list of the nodes from root to child
+	 */
 	public List<V> getPath(V child) {
 		List<V> list = new ArrayList<V>();
 		list.add(child);
@@ -408,22 +482,49 @@ public class SimpleSparseTree<V,E> implements Tree<V,E>, DirectedGraph<V,E> {
 		return list;
 	}
 
+	/**
+	 * getter for the root of the tree
+	 * @return the root
+	 */
 	public V getRoot() {
 		return root;
 	}
 	
+	/**
+	 * sets the root to the passed value, only if the root is
+	 * previously unset
+	 * @param root the initial tree root
+	 */
 	public void setRoot(V root) {
 		addVertex(root);
 	}
 
+	/**
+	 * removes a node from the tree, causing all descendants of
+	 * the removed node also to be removed
+	 * @param orphan the node to remove
+	 * @return whether this call mutates the underlying graph
+	 */
 	public boolean removeChild(V orphan) {
 		return removeVertex(orphan);
 	}
 
+	/**
+	 * computes and returns the depth of the tree from the
+	 * root to the passed vertex
+	 * 
+	 * @param v the node who's depth is computed
+	 * @return the depth to the passed node.
+	 */
 	public int getDepth(V v) {
 		return getPath(v).size();
 	}
 
+	/**
+	 * computes and returns the height of the tree
+	 * 
+	 * @return the height
+	 */
 	public int getHeight() {
 		int height = 0;
 		for(V v : getVertices()) {
@@ -432,17 +533,32 @@ public class SimpleSparseTree<V,E> implements Tree<V,E>, DirectedGraph<V,E> {
 		return height;
 	}
 
+	/**
+	 * computes and returns whether the passed node is
+	 * neither the root, nor a leaf node.
+	 * @return 
+	 */
 	public boolean isInternal(V v) {
 		return isLeaf(v) == false && isRoot(v) == false;
 	}
 
+	/**
+	 * computes and returns whether the passed node is
+	 * a leaf (has no child nodes)
+	 */
 	public boolean isLeaf(V v) {
 		return getChildren(v).size() == 0;
 	}
 
+	/**
+	 * computes whether the passed node is a root node
+	 * (has no children)
+	 */
 	public boolean isRoot(V v) {
 		return getParent(v) == null;
 	}
-	
 
+	public Collection<V> getRoots() {
+		return Collections.singleton(getRoot());
+	}
 }
