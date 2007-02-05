885835084afaa479b934b1975cb1b89c987c497b
e-flat <e-flat>
2007-02-05 06:42:36 +0000

    modified to reduce overhead (now uses object references internally instead of a couple of ArrayLists)

--- jung-api/src/main/java/edu/uci/ics/graph/util/Pair.java
+++ jung-api/src/main/java/edu/uci/ics/graph/util/Pair.java
@@ -11,11 +11,8 @@
  */
 package edu.uci.ics.graph.util;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 
 
 /**
@@ -31,15 +28,19 @@ import java.util.List;
 
 public final class Pair<T> implements Collection<T> 
 {
-    private List<T> list = new ArrayList<T>(2);
-    private final List<T> delegate;
+//    private List<T> list = new ArrayList<T>(2);
+//    private final List<T> delegate;
+    private T first;
+    private T second;
 
     public Pair(T value1, T value2) 
     {
         assert value1 != null && value2 != null : "Both inputs must be non-null";
-        list.add(value1);
-        list.add(value2);
-        delegate = Collections.unmodifiableList(list);
+        first = value1;
+        second = value2;
+//        list.add(value1);
+//        list.add(value2);
+//        delegate = Collections.unmodifiableList(list);
     }
     
     /**
@@ -49,12 +50,22 @@ public final class Pair<T> implements Collection<T>
      */
     public Pair(Collection<T> values) 
     {
-    	if(values.size() == 1 || values.size() == 2) {
-    		list.addAll(values);
-    		delegate = Collections.unmodifiableList(list);
-    	} else {
-    		throw new IllegalArgumentException("Pair may only be created from a Collection of 1 or 2 elements");
-    	}
+        if (values.size() == 2)
+        {
+            Iterator<T> iter = values.iterator();
+            first = iter.next();
+            second = iter.next();
+        }
+        else
+            throw new IllegalArgumentException("Pair may only be created from a Collection of 2 elements");
+        
+//    	if(values.size() == 1 || values.size() == 2) {
+//    		list.addAll(values);
+//    		delegate = Collections.unmodifiableList(list);
+//    	} 
+//        else {
+//    		throw new IllegalArgumentException("Pair may only be created from a Collection of 1 or 2 elements");
+//    	}
     }
 
     /**
@@ -62,7 +73,8 @@ public final class Pair<T> implements Collection<T>
      */
     public T getFirst() 
     {
-        return delegate.get(0);
+//        return delegate.get(0);
+        return first;
     }
     
     /**
@@ -70,7 +82,8 @@ public final class Pair<T> implements Collection<T>
      */
     public T getSecond() 
     {
-        return delegate.get(1);
+//        return delegate.get(1);
+        return second;
     }
     
     /**
@@ -79,18 +92,32 @@ public final class Pair<T> implements Collection<T>
     public boolean equals( Object o ) {
         if (o == this)
             return true;
-        if (!(o instanceof Pair))
-            return false;
 
-        Iterator<T> e1 = delegate.iterator();
-        Iterator e2 = ((Pair) o).iterator();
-        while(e1.hasNext() && e2.hasNext()) {
-            T o1 = e1.next();
-            Object o2 = e2.next();
-            if (!(o1==null ? o2==null : o1.equals(o2)))
-                return false;
+        if (o instanceof Pair) {
+            Pair tt = (Pair) o;
+            Object first = tt.getFirst();
+            Object second = tt.getSecond();
+            return ((first == this.first || first.equals(this.first)) &&
+                    (second == this.second || second.equals(this.second)));
+        } else {
+            return false;
         }
-        return !(e1.hasNext() || e2.hasNext());
+
+        
+        // FIXME: should we allow other types of Collections?
+//        if (!(o instanceof Pair))
+//            return false;
+
+        
+//        Iterator<T> e1 = delegate.iterator();
+//        Iterator e2 = ((Pair) o).iterator();
+//        while(e1.hasNext() && e2.hasNext()) {
+//            T o1 = e1.next();
+//            Object o2 = e2.next();
+//            if (!(o1==null ? o2==null : o1.equals(o2)))
+//                return false;
+//        }
+//        return !(e1.hasNext() || e2.hasNext());
     }
     
     /**
@@ -98,7 +125,9 @@ public final class Pair<T> implements Collection<T>
      */
     public int hashCode() 
     {
-        return delegate.hashCode();
+        // FIXME: do we need something more robust here?
+        return first.hashCode() + second.hashCode();
+//        return delegate.hashCode();
     }
     
     /**
@@ -106,59 +135,123 @@ public final class Pair<T> implements Collection<T>
      */
     public String toString()
     {
-        return delegate.toString();
+        return "<" + first.toString() + ", " + second.toString() + ">";
+//        return delegate.toString();
     }
 
     public boolean add(T o) {
-        return delegate.add(o);
+        throw new UnsupportedOperationException("Pairs cannot be mutated");
+//        return delegate.add(o);
     }
 
     public boolean addAll(Collection<? extends T> c) {
-        return delegate.addAll(c);
+        throw new UnsupportedOperationException("Pairs cannot be mutated");
+//        return delegate.addAll(c);
     }
 
     public void clear() {
-        delegate.clear();
+        throw new UnsupportedOperationException("Pairs cannot be mutated");
+//        delegate.clear();
     }
 
     public boolean contains(Object o) {
-        return delegate.contains(o);
+        return (first == o || first.equals(o) || second == o || second.equals(o));
+//        return delegate.contains(o);
     }
 
     public boolean containsAll(Collection<?> c) {
-        return delegate.containsAll(c);
+        if (c.size() > 2)
+            return false;
+        Iterator iter = c.iterator();
+        Object c_first = iter.next();
+        Object c_second = iter.next();
+        return this.contains(c_first) && this.contains(c_second);
+//        return delegate.containsAll(c);
     }
 
     public boolean isEmpty() {
-        return delegate.isEmpty();
+        return false;
+//        return delegate.isEmpty();
     }
 
     public Iterator<T> iterator() {
-        return delegate.iterator();
+        return new PairIterator();
     }
 
     public boolean remove(Object o) {
-        return delegate.remove(o);
+        throw new UnsupportedOperationException("Pairs cannot be mutated");
+//        return delegate.remove(o);
     }
 
     public boolean removeAll(Collection<?> c) {
-        return delegate.removeAll(c);
+        throw new UnsupportedOperationException("Pairs cannot be mutated");
+//        return delegate.removeAll(c);
     }
 
     public boolean retainAll(Collection<?> c) {
-        return delegate.retainAll(c);
+        throw new UnsupportedOperationException("Pairs cannot be mutated");
+//        return delegate.retainAll(c);
     }
 
     public int size() {
-        return delegate.size();
+        return 2;
+//        return delegate.size();
     }
 
     public Object[] toArray() {
-        return delegate.toArray();
+        Object[] to_return = new Object[2];
+        to_return[0] = first;
+        to_return[1] = second;
+        return to_return;
+        
+//        return delegate.toArray();
     }
 
     public <S> S[] toArray(S[] a) {
-        return delegate.<S>toArray(a);
+        S[] to_return = a;
+        Class<?> type = a.getClass().getComponentType();
+//        if (!(T instanceof S))
+//            throw new ArrayStoreException("input type is not a supertype of this Pair's type");
+        if (a.length < 2)
+            to_return = (S[])java.lang.reflect.Array.newInstance(type, 2);
+        to_return[0] = (S)first;
+        to_return[1] = (S)second;
+        
+        if (to_return.length > 2)
+            to_return[2] = null;
+        return to_return;
+//        return delegate.<S>toArray(a);
+    }
+    
+    private class PairIterator implements Iterator
+    {
+        int position;
+        
+        private PairIterator()
+        {
+            position = 0;
+        }
+
+        public boolean hasNext()
+        {
+            return position < 2;
+        }
+
+        public Object next()
+        {
+            position++;
+            if (position == 1)
+                return first;
+            else if (position == 2)
+                return second;
+            else
+                return null;
+        }
+
+        public void remove()
+        {
+            throw new UnsupportedOperationException("Pairs cannot be mutated");
+        }
     }
 }
 
