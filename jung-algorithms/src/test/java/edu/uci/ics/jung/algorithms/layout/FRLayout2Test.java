1b64983e611539dec48f64ad86b626377f72e246
tomnelson <tomnelson>
2007-02-07 01:44:10 +0000

    tests to compare time of FRLayout2 and FRLayout

---
+++ jung-algorithms/src/test/java/edu/uci/ics/jung/algorithms/layout/FRLayout2Test.java
@@ -0,0 +1,29 @@
+package edu.uci.ics.jung.algorithms.layout;
+
+import java.awt.Dimension;
+import java.util.HashSet;
+import java.util.Set;
+
+import junit.framework.TestCase;
+import edu.uci.ics.graph.Graph;
+import edu.uci.ics.jung.algorithms.IterativeContext;
+import edu.uci.ics.jung.graph.generators.random.TestGraphs;
+
+public class FRLayout2Test extends TestCase {
+	
+    protected Set<Integer> seedVertices = new HashSet<Integer>();
+
+	public void testFRLayout() {
+		
+		Graph<String,Number> graph = TestGraphs.getOneComponentGraph();
+
+		Layout<String,Number> layout = new FRLayout2<String,Number>(graph);
+		layout.setSize(new Dimension(600,600));
+		if(layout instanceof IterativeContext) {
+			layout.initialize();
+			Relaxer relaxer = new VisRunner((IterativeContext)layout);
+			relaxer.prerelax();
+			relaxer.relax();
+		}
+	}
+}
