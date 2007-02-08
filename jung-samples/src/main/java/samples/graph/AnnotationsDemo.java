2ab5edf4d1c56a86c60447816541bc5207195671
tomnelson <tomnelson>
2007-02-08 02:12:38 +0000

    first commitnew demo of graph viz annotations

---
+++ jung-samples/src/main/java/samples/graph/AnnotationsDemo.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright (c) 2003, the JUNG Project and the Regents of the University of
+ * California All rights reserved.
+ * 
+ * This software is open-source under the BSD license; see either "license.txt"
+ * or http://jung.sourceforge.net/license.txt for a description.
+ * 
+ */
+package samples.graph;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import edu.uci.ics.graph.Graph;
+import edu.uci.ics.jung.algorithms.layout.FRLayout;
+import edu.uci.ics.jung.graph.generators.random.TestGraphs;
+import edu.uci.ics.jung.visualization.DefaultVisualizationModel;
+import edu.uci.ics.jung.visualization.GraphZoomScrollPane;
+import edu.uci.ics.jung.visualization.RenderContext;
+import edu.uci.ics.jung.visualization.VisualizationModel;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.VisualizationServer.Paintable;
+import edu.uci.ics.jung.visualization.annotations.AnnotatingGraphMousePlugin;
+import edu.uci.ics.jung.visualization.annotations.AnnotatingModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.CrossoverScalingControl;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.ScalingControl;
+import edu.uci.ics.jung.visualization.decorators.PickableEdgePaintTransformer;
+import edu.uci.ics.jung.visualization.decorators.PickableVertexPaintTransformer;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import edu.uci.ics.jung.visualization.renderers.Renderer;
+
+/**
+ * 
+ * @author Tom Nelson
+ * 
+ */
+public class AnnotationsDemo<V, E> extends JApplet {
+
+//    static final String instructions = 
+//        "<html>"+
+//        "<b><h2><center>Instructions for Mouse Listeners</center></h2></b>"+
+//        "<p>There are two modes, Transforming and Picking."+
+//        "<p>The modes are selected with a combo box."+
+//        
+//        "<p><p><b>Transforming Mode:</b>"+
+//        "<ul>"+
+//        "<li>Mouse1+drag pans the graph"+
+//        "<li>Mouse1+Shift+drag rotates the graph"+
+//        "<li>Mouse1+CTRL(or Command)+drag shears the graph"+
+//        "</ul>"+
+//        
+//        "<b>Picking Mode:</b>"+
+//        "<ul>"+
+//        "<li>Mouse1 on a Vertex selects the vertex"+
+//        "<li>Mouse1 elsewhere unselects all Vertices"+
+//        "<li>Mouse1+Shift on a Vertex adds/removes Vertex selection"+
+//        "<li>Mouse1+drag on a Vertex moves all selected Vertices"+
+//        "<li>Mouse1+drag elsewhere selects Vertices in a region"+
+//        "<li>Mouse1+Shift+drag adds selection of Vertices in a new region"+
+//        "<li>Mouse1+CTRL on a Vertex selects the vertex and centers the display on it"+
+//        "</ul>"+
+//       "<b>Both Modes:</b>"+
+//       "<ul>"+
+//        "<li>Mousewheel scales with a crossover value of 1.0.<p>"+
+//        "     - scales the graph layout when the combined scale is greater than 1<p>"+
+//        "     - scales the graph view when the combined scale is less than 1";
+    
+    JDialog helpDialog;
+    
+    Paintable viewGrid;
+    
+    /**
+     * create an instance of a simple graph in two views with controls to
+     * demo the features.
+     * 
+     */
+    public AnnotationsDemo() {
+        
+        // create a simple graph for the demo
+        Graph<String, Number> graph = TestGraphs.getOneComponentGraph();
+        
+        // the preferred sizes for the two views
+        Dimension preferredSize1 = new Dimension(600,600);
+        
+        // create one layout for the graph
+        FRLayout<String,Number> layout = new FRLayout<String,Number>(graph);
+        layout.setMaxIterations(500);
+        
+        VisualizationModel<String,Number> vm =
+            new DefaultVisualizationModel<String,Number>(layout, preferredSize1);
+        
+        // create 2 views that share the same model
+        final VisualizationViewer<String,Number> vv = 
+            new VisualizationViewer<String,Number>(vm, preferredSize1);
+        vv.setBackground(Color.white);
+        vv.getRenderContext().setEdgeDrawPaintTransformer(new PickableEdgePaintTransformer<String,Number>(vv.getPickedEdgeState(), Color.black, Color.cyan));
+        vv.getRenderContext().setVertexFillPaintTransformer(new PickableVertexPaintTransformer<String>(vv.getPickedVertexState(), Color.red, Color.yellow));
+        vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+        vv.getRenderer().getVertexLabelRenderer().setPosition(Renderer.VertexLabel.Position.CNTR);
+        
+        // add default listener for ToolTips
+        vv.setVertexToolTipTransformer(new ToStringLabeller());
+        
+//        ToolTipManager.sharedInstance().setDismissDelay(10000);
+        
+        Container content = getContentPane();
+        Container panel = new JPanel(new BorderLayout());
+        
+        GraphZoomScrollPane gzsp = new GraphZoomScrollPane(vv);
+        panel.add(gzsp);
+        
+        helpDialog = new JDialog();
+//        helpDialog.getContentPane().add(new JLabel(instructions));
+        
+        RenderContext rc = vv.getRenderContext();
+        AnnotatingGraphMousePlugin annotatingPlugin =
+        	new AnnotatingGraphMousePlugin(rc);
+        // create a GraphMouse for the main view
+        // 
+        final AnnotatingModalGraphMouse graphMouse = 
+        	new AnnotatingModalGraphMouse(rc, annotatingPlugin);
+        vv.setGraphMouse(graphMouse);
+        vv.addKeyListener(graphMouse.getModeKeyListener());
+        
+        final ScalingControl scaler = new CrossoverScalingControl();
+
+        JButton plus = new JButton("+");
+        plus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                scaler.scale(vv, 1.1f, vv.getCenter());
+            }
+        });
+        JButton minus = new JButton("-");
+        minus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                scaler.scale(vv, 1/1.1f, vv.getCenter());
+            }
+        });
+        
+        JComboBox modeBox = graphMouse.getModeComboBox();
+        modeBox.setSelectedItem(ModalGraphMouse.Mode.ANNOTATING);
+        
+        JButton help = new JButton("Help");
+        help.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                helpDialog.pack();
+                helpDialog.setVisible(true);
+            }
+        });
+
+        JPanel controls = new JPanel();
+        controls.add(plus);
+        controls.add(minus);
+        controls.add(graphMouse.getModeComboBox());
+        controls.add(graphMouse.getShapeBox());
+        controls.add(graphMouse.getColorChooserButton());
+        controls.add(help);
+        content.add(panel);
+        content.add(controls, BorderLayout.SOUTH);
+    }
+    
+    /**
+     * a driver for this demo
+     */
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().add(new AnnotationsDemo());
+        f.pack();
+        f.setVisible(true);
+    }
+}
