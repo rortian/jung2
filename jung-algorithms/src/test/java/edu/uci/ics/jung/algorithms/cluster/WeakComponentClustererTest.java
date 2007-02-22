01109b0baf43b74475904b97c0c3b7c223f73741
tomnelson <tomnelson>
2007-02-22 15:17:27 +0000

    test without factory

--- jung-algorithms/src/test/java/edu/uci/ics/jung/algorithms/cluster/WeakComponentClustererTest.java
+++ jung-algorithms/src/test/java/edu/uci/ics/jung/algorithms/cluster/WeakComponentClustererTest.java
@@ -2,25 +2,17 @@ package edu.uci.ics.jung.algorithms.cluster;
 
 import java.util.Set;
 
-import org.apache.commons.collections15.Factory;
-
-import edu.uci.ics.graph.Graph;
-import edu.uci.ics.jung.graph.SparseGraph;
-import edu.uci.ics.jung.graph.util.TestGraphs;
 import junit.framework.TestCase;
+import edu.uci.ics.graph.Graph;
+import edu.uci.ics.jung.graph.util.TestGraphs;
 
 public class WeakComponentClustererTest extends TestCase {
 	
 	Graph<String,Number> graph =  TestGraphs.getDemoGraph();
-	Factory<Graph<String,Number>> factory = new Factory<Graph<String,Number>>() {
-
-		public Graph<String, Number> create() {
-			return new SparseGraph<String,Number>();
-		}};
 	
 	public void testWeakComponent() {
 		WeakComponentGraphClusterer<String,Number> clusterer = 
-			new WeakComponentGraphClusterer<String,Number>(factory);
+			new WeakComponentGraphClusterer<String,Number>();
 		Set<Graph<String,Number>> clusterSet = clusterer.transform(graph);
 		System.err.println("set is "+clusterSet);
 	}
