b1835e512cfb55c3c3554c193b1095e886469df8
tomnelson <tomnelson>
2007-05-05 20:51:02 +0000

    tests pass now

--- jung-algorithms/src/test/java/edu/uci/ics/jung/algorithms/importance/TestBetweennessCentrality.java
+++ jung-algorithms/src/test/java/edu/uci/ics/jung/algorithms/importance/TestBetweennessCentrality.java
@@ -9,13 +9,13 @@
 */
 package edu.uci.ics.jung.algorithms.importance;
 
+import junit.framework.Assert;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import org.apache.commons.collections15.BidiMap;
 
-import edu.uci.ics.jung.algorithms.Indexer;
 import edu.uci.ics.jung.graph.DirectedGraph;
 import edu.uci.ics.jung.graph.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.Graph;
@@ -45,44 +45,46 @@ public class TestBetweennessCentrality extends TestCase {
         	graph.addVertex(i);
         }
 
-		BidiMap<Integer,Integer> id = Indexer.create(graph.getVertices());
 		int edge = 0;
-        graph.addEdge(edge++, id.get(0),id.get(1));
-        graph.addEdge(edge++, id.get(0),id.get(6));
-        graph.addEdge(edge++, id.get(1),id.get(2));
-        graph.addEdge(edge++, id.get(1),id.get(3));
-        graph.addEdge(edge++, id.get(2),id.get(4));
-        graph.addEdge(edge++, id.get(3),id.get(4));
-        graph.addEdge(edge++, id.get(4),id.get(5));
-        graph.addEdge(edge++, id.get(5),id.get(8));
-        graph.addEdge(edge++, id.get(7),id.get(8));
-        graph.addEdge(edge++, id.get(6),id.get(7));
+		graph.addEdge(edge++, 0,1);
+		graph.addEdge(edge++, 0,6);
+		graph.addEdge(edge++, 1,2);
+		graph.addEdge(edge++, 1,3);
+		graph.addEdge(edge++, 2,4);
+		graph.addEdge(edge++, 3,4);
+		graph.addEdge(edge++, 4,5);
+		graph.addEdge(edge++, 5,8);
+		graph.addEdge(edge++, 7,8);
+		graph.addEdge(edge++, 6,7);
 
-//        BetweennessCentrality<Integer,Integer> bc = 
-//        	new BetweennessCentrality<Integer,Integer>(graph);
-//        bc.setRemoveRankScoresOnFinalize(false);
-//        bc.evaluate();
+        BetweennessCentrality<Integer,Integer> bc = 
+        	new BetweennessCentrality<Integer,Integer>(graph);
+        bc.setRemoveRankScoresOnFinalize(false);
+        bc.evaluate();
 
-//        Assert.assertEquals(bc.getVertexRankScore(id.get(0))/28.0,0.2142,.001);
-//        Assert.assertEquals(bc.getVertexRankScore(id.get(1))/28.0,0.2797,.001);
-//        Assert.assertEquals(bc.getVertexRankScore(id.get(2))/28.0,0.0892,.001);
-//        Assert.assertEquals(bc.getVertexRankScore(id.get(3))/28.0,0.0892,.001);
-//        Assert.assertEquals(bc.getVertexRankScore(id.get(4))/28.0,0.2797,.001);
-//        Assert.assertEquals(bc.getVertexRankScore(id.get(5))/28.0,0.2142,.001);
-//        Assert.assertEquals(bc.getVertexRankScore(id.get(6))/28.0,0.1666,.001);
-//        Assert.assertEquals(bc.getVertexRankScore(id.get(7))/28.0,0.1428,.001);
-//        Assert.assertEquals(bc.getVertexRankScore(id.get(8))/28.0,0.1666,.001);
-//
-//        Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,0,1,id)),10.66666,.001);
-//        Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,0,6,id)),9.33333,.001);
-//        Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,1,2,id)),6.5,.001);
-//        Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,1,3,id)),6.5,.001);
-//        Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,2,4,id)),6.5,.001);
-//        Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,3,4,id)),6.5,.001);
-//        Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,4,5,id)),10.66666,.001);
-//        Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,5,8,id)),9.33333,.001);
-//        Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,6,7,id)),8.0,.001);
-//        Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,7,8,id)),8.0,.001);
+        Assert.assertEquals(bc.getVertexRankScore(0)/28.0,0.2142,.001);
+        Assert.assertEquals(bc.getVertexRankScore(1)/28.0,0.2797,.001);
+        Assert.assertEquals(bc.getVertexRankScore(2)/28.0,0.0892,.001);
+        Assert.assertEquals(bc.getVertexRankScore(3)/28.0,0.0892,.001);
+        Assert.assertEquals(bc.getVertexRankScore(4)/28.0,0.2797,.001);
+        Assert.assertEquals(bc.getVertexRankScore(5)/28.0,0.2142,.001);
+        Assert.assertEquals(bc.getVertexRankScore(6)/28.0,0.1666,.001);
+        Assert.assertEquals(bc.getVertexRankScore(7)/28.0,0.1428,.001);
+        Assert.assertEquals(bc.getVertexRankScore(8)/28.0,0.1666,.001);
+
+        Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(0,1)),
+        		10.66666,.001);
+
+        Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(0,1)),10.66666,.001);
+        Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(0,6)),9.33333,.001);
+        Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(1,2)),6.5,.001);
+        Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(1,3)),6.5,.001);
+        Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(2,4)),6.5,.001);
+        Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(3,4)),6.5,.001);
+        Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(4,5)),10.66666,.001);
+        Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(5,8)),9.33333,.001);
+        Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(6,7)),8.0,.001);
+        Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(7,8)),8.0,.001);
     }
     
     public void testRankerDirected() {
@@ -90,27 +92,27 @@ public class TestBetweennessCentrality extends TestCase {
     	for(int i=0; i<5; i++) {
     		graph.addVertex(i);
     	}
-    	BidiMap<Integer,Integer> id = Indexer.create(graph.getVertices());
+
     	int edge=0;
-    	graph.addEdge(edge++, id.get(0),id.get(1));
-    	graph.addEdge(edge++, id.get(1),id.get(2));
-    	graph.addEdge(edge++, id.get(3),id.get(1));
-    	graph.addEdge(edge++, id.get(4),id.get(2));
+    	graph.addEdge(edge++, 0,1);
+    	graph.addEdge(edge++, 1,2);
+    	graph.addEdge(edge++, 3,1);
+    	graph.addEdge(edge++, 4,2);
 
-//    	BetweennessCentrality<Integer,Integer> bc = 
-//    		new BetweennessCentrality<Integer,Integer>(graph);
-//    	bc.setRemoveRankScoresOnFinalize(false);
-//    	bc.evaluate();
+    	BetweennessCentrality<Integer,Integer> bc = 
+    		new BetweennessCentrality<Integer,Integer>(graph);
+    	bc.setRemoveRankScoresOnFinalize(false);
+    	bc.evaluate();
 
-//    	Assert.assertEquals(bc.getVertexRankScore(id.get(0)),0,.001);
-//    	Assert.assertEquals(bc.getVertexRankScore(id.get(1)),2,.001);
-//    	Assert.assertEquals(bc.getVertexRankScore(id.get(2)),0,.001);
-//    	Assert.assertEquals(bc.getVertexRankScore(id.get(3)),0,.001);
-//    	Assert.assertEquals(bc.getVertexRankScore(id.get(4)),0,.001);
-//
-//    	Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,0,1,id)),2,.001);
-//    	Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,1,2,id)),3,.001);
-//    	Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,3,1,id)),2,.001);
-//    	Assert.assertEquals(bc.getEdgeRankScore(TestBetweennessCentrality.<Integer,Integer>getEdge(graph,4,2,id)),1,.001);
+    	Assert.assertEquals(bc.getVertexRankScore(0),0,.001);
+    	Assert.assertEquals(bc.getVertexRankScore(1),2,.001);
+    	Assert.assertEquals(bc.getVertexRankScore(2),0,.001);
+    	Assert.assertEquals(bc.getVertexRankScore(3),0,.001);
+    	Assert.assertEquals(bc.getVertexRankScore(4),0,.001);
+
+    	Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(0,1)),2,.001);
+    	Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(1,2)),3,.001);
+    	Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(3,1)),2,.001);
+    	Assert.assertEquals(bc.getEdgeRankScore(graph.findEdge(4,2)),1,.001);
     }
 }
