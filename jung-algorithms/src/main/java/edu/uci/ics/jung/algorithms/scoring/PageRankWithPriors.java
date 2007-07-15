3caa8ca9ef33fe1e0d1c3385180118969ddf20ff
e-flat <e-flat>
2007-07-15 10:27:17 +0000

    simplified code; moved some into abstract 'priors' class

--- jung-algorithms/src/main/java/edu/uci/ics/jung/algorithms/scoring/PageRankWithPriors.java
+++ jung-algorithms/src/main/java/edu/uci/ics/jung/algorithms/scoring/PageRankWithPriors.java
@@ -14,34 +14,15 @@ package edu.uci.ics.jung.algorithms.scoring;
 import java.util.Map;
 
 import org.apache.commons.collections15.Transformer;
-import org.apache.commons.collections15.functors.MapTransformer;
 
 import edu.uci.ics.jung.graph.Graph;
 
-public class PageRankWithPriors<V, E> extends AbstractIterativeScorer<V, E, Double> implements VertexScorer<V, Double>
+public class PageRankWithPriors<V, E> extends AbstractIterativeScorerWithPriors<V, E, Double>
 {
-    /**
-     * 
-     */
-    protected double alpha;
-    
-    /**
-     * 
-     */
-    protected Transformer<V, Double> vertex_priors;
-    
-    /**
-     * True if this instance permits 'sink' vertices (those with outdegree 0) and adjusts the results 
-     * accordingly (by distributing incoming potential according to the specified vertex priors, i.e., 
-     * uniformly by default), and false if this instance rejects sink vertices and throws an exception if 
-     * one is encountered.
-     */
-    protected boolean accept_sinks;
-
     public PageRankWithPriors(Graph<V,E> graph, Transformer<E, ? extends Number> edge_weights, 
             Transformer<V, Double> vertex_priors, double alpha)
     {
-        super(graph, edge_weights);
+        super(graph, edge_weights, vertex_priors, alpha);
     }
     
     /**
@@ -52,27 +33,7 @@ public class PageRankWithPriors<V, E> extends AbstractIterativeScorer<V, E, Doub
      */
     public PageRankWithPriors(Graph<V,E> graph, Transformer<V, Double> vertex_priors, double alpha)
     {
-        super(graph);
-        this.alpha = alpha;
-        this.vertex_priors = vertex_priors;
-        
-        // initialize output to priors
-//        this.user_output = output_map;
-        for (V v : graph.getVertices())
-            output.put(v, vertex_priors.transform(v));
-
-        
-        this.accept_sinks = true;
-    }
-    
-    /**
-     * 
-     * @param graph
-     * @param alpha
-     */
-    public PageRankWithPriors(Graph<V,E> graph, Transformer<V, Double> vertex_priors)
-    {
-        this(graph, vertex_priors, 0);
+        super(graph, vertex_priors, alpha);
     }
     
     /**
@@ -84,24 +45,13 @@ public class PageRankWithPriors<V, E> extends AbstractIterativeScorer<V, E, Doub
         
         for (V v : graph.getVertices())
         {
-            if (graph.outDegree(v) == 0)
-            {
-                if (accept_sinks)
-                    disappearing_potential += output.get(v).doubleValue();
-                else
-                    throw new IllegalArgumentException("Input graph vertices must all have outdegree > 0");
-            }
+            disappearing_potential += getDisappearingPotential(v);
             
             double total_input = 0;
             for (E e : graph.getInEdges(v))
             {
                 V w = graph.getOpposite(v, e);
-                double value;
-                if (edge_weights instanceof VertexEdgeWeight)
-                    value = ((VertexEdgeWeight<V,E,? extends Number>)edge_weights).transform(new VEPair<V,E>(w,e)).doubleValue();
-                else
-                    value = edge_weights.transform(e).doubleValue();
-                total_input += (output.get(w).doubleValue() * value);
+                total_input += (output.get(w).doubleValue() * getEdgeWeight(w,e).doubleValue());
             }
             
             // modify total_input according to alpha
@@ -115,12 +65,11 @@ public class PageRankWithPriors<V, E> extends AbstractIterativeScorer<V, E, Doub
         // distribute disappearing potential according to priors
         if (disappearing_potential > 0)
         {
-//            double to_add = (1 - alpha) * (disappearing_potential / graph.getVertexCount());
             for (V v : graph.getVertices())
             {
-                double value = current_values.get(v).doubleValue();
-                double to_add = (1 - alpha) * (disappearing_potential * vertex_priors.transform(v));
-                current_values.put(v, value + to_add);
+                setVertexScore(v, 
+                        getVertexScore(v) + 
+                        (1 - alpha) * (disappearing_potential * vertex_priors.transform(v)));
             }
         }
         
@@ -131,45 +80,15 @@ public class PageRankWithPriors<V, E> extends AbstractIterativeScorer<V, E, Doub
         total_iterations++;
     }
 
-    public void setAcceptSinks(boolean accept)
+    protected Double getDisappearingPotential(V v)
     {
-        this.accept_sinks = accept;
+        if (graph.outDegree(v) == 0)
+        {
+            if (accept_disconnected_graph)
+                return output.get(v);
+            else
+                throw new IllegalArgumentException("Outdegree of " + v + " must be > 0");
+        }
+        return 0.0;
     }
-    
-    public boolean getAcceptSinks()
-    {
-        return this.accept_sinks;
-    }
-    
-    public Transformer<V, Double> getVertexScores()
-    {
-//        if (user_output != output)
-//        {
-//            // copy output into user-specified storage if output is currently in the storage
-//            // allocated to current values (current values and output swap back and forth over the
-//            // course of the algorithm to save space and reallocation time)
-//            for (Map.Entry<V, Double> entry : output.entrySet())
-//            {
-//                user_output.put(entry.getKey(), entry.getValue());
-//            }
-//        }
-        return MapTransformer.getInstance(output);
-    }
-
-    /**
-     * @return the vertex_priors
-     */
-    public Transformer<V, Double> getVertexPriors()
-    {
-        return vertex_priors;
-    }
-
-    /**
-     * @return the alpha
-     */
-    public double getAlpha()
-    {
-        return alpha;
-    }
-
 }
