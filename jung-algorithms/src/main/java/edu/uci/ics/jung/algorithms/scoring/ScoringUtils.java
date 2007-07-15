37566ca091671d94ba47e7ecc651815758b3308d
e-flat <e-flat>
2007-07-15 10:26:36 +0000

    refactored; added two-value uniform prior generator

--- jung-algorithms/src/main/java/edu/uci/ics/jung/algorithms/scoring/ScoringUtils.java
+++ jung-algorithms/src/main/java/edu/uci/ics/jung/algorithms/scoring/ScoringUtils.java
@@ -15,24 +15,10 @@ import java.util.Collection;
 
 import org.apache.commons.collections15.Transformer;
 
-import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.util.Pair;
 
 public class ScoringUtils
 {
-    public static <V> Transformer<V, Double> getUniformPrior(Graph<V,?> g)
-    {
-        final Graph<V, ?> inner_graph = g;
-        Transformer<V, Double> distribution = new Transformer<V, Double>()
-        {
-            public Double transform(V input)
-            {
-                return 1.0 / inner_graph.getVertexCount();
-            }
-        };
-        return distribution;
-    }
-
-    
     public static <V> Transformer<V, Double> getUniformRootPrior(Collection<V> roots)
     {
         final Collection<V> inner_roots = roots;
@@ -49,4 +35,22 @@ public class ScoringUtils
         
         return distribution;
     }
+    
+    public static <V> Transformer<V, Pair<Double>> getTwoValueUniformRootPrior(Collection<V> roots)
+    {
+        final Collection<V> inner_roots = roots;
+        Transformer<V, Pair<Double>> distribution = new Transformer<V, Pair<Double>>()
+        {
+            public Pair<Double> transform(V input)
+            {
+                if (inner_roots.contains(input))
+                    return new Pair<Double>(1.0 / inner_roots.size(), 1.0 / inner_roots.size());
+                else
+                    return new Pair<Double>(0.0, 0.0);
+            }
+        };
+        
+        return distribution;
+    }
+
 }
