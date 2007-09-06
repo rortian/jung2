23b86f524720dbef9a0817cf171bc620e5197e41
e-flat <e-flat>
2007-09-06 08:00:08 +0000

    import cleanup

--- jung-io/src/main/java/edu/uci/ics/jung/io/PajekNetReader.java
+++ jung-io/src/main/java/edu/uci/ics/jung/io/PajekNetReader.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 import org.apache.commons.collections15.Factory;
