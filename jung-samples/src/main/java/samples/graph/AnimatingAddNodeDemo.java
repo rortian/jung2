fc19d7ee6531006153afc4f5a49996c360631d15
tomnelson <tomnelson>
2008-03-30 18:58:19 +0000

    removed unused import

--- jung-samples/src/main/java/samples/graph/AnimatingAddNodeDemo.java
+++ jung-samples/src/main/java/samples/graph/AnimatingAddNodeDemo.java
@@ -17,7 +17,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.util.Timer;
 import java.util.TimerTask;
 
