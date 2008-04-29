dc4f2bfc05420cfdf7418bb09217b2b6883cea1f
tomnelson <tomnelson>
2008-04-29 00:54:26 +0000

    simplified

--- jung-api/src/main/java/edu/uci/ics/jung/graph/util/DefaultParallelEdgeIndexFunction.java
+++ jung-api/src/main/java/edu/uci/ics/jung/graph/util/DefaultParallelEdgeIndexFunction.java
@@ -35,8 +35,7 @@ import edu.uci.ics.jung.graph.Graph;
  */
 public class DefaultParallelEdgeIndexFunction<V,E> implements EdgeIndexFunction<V,E>
 {
-    protected Map<Context<Graph<V,E>,E>, Integer> edge_index = 
-    	new HashMap<Context<Graph<V,E>,E>, Integer>();
+    protected Map<Context<Graph<V,E>,E>, Integer> edge_index = new HashMap<Context<Graph<V,E>,E>, Integer>();
     
     private DefaultParallelEdgeIndexFunction() {
     }
@@ -52,27 +51,28 @@ public class DefaultParallelEdgeIndexFunction<V,E> implements EdgeIndexFunction<
      */
     public int getIndex(Graph<V,E> graph, E e)
     {
-    	Context<Graph<V,E>,E> context = Context.<Graph<V,E>,E>getInstance(graph,e);
-        Integer index = edge_index.get(context);
+    	
+        Integer index = edge_index.get(Context.<Graph<V,E>,E>getInstance(graph,e));
+        	//edge_index.get(e);
         if(index == null) {
         	Pair<V> endpoints = graph.getEndpoints(e);
         	V u = endpoints.getFirst();
         	V v = endpoints.getSecond();
         	if(u.equals(v)) {
-        		index = getIndex(context, v);
+        		index = getIndex(graph, e, v);
         	} else {
-        		index = getIndex(context, u, v);
+        		index = getIndex(graph, e, u, v);
         	}
         }
         return index.intValue();
     }
 
-    protected int getIndex(Context<Graph<V,E>,E> context, V v, V u) {
-    	Collection<E> commonEdgeSet = new HashSet<E>(context.graph.getIncidentEdges(u));
-    	commonEdgeSet.retainAll(context.graph.getIncidentEdges(v));
+    protected int getIndex(Graph<V,E> graph, E e, V v, V u) {
+    	Collection<E> commonEdgeSet = new HashSet<E>(graph.getIncidentEdges(u));
+    	commonEdgeSet.retainAll(graph.getIncidentEdges(v));
     	for(Iterator<E> iterator=commonEdgeSet.iterator(); iterator.hasNext(); ) {
     		E edge = iterator.next();
-    		Pair<V> ep = context.graph.getEndpoints(edge);
+    		Pair<V> ep = graph.getEndpoints(edge);
     		V first = ep.getFirst();
     		V second = ep.getSecond();
     		// remove loops
@@ -86,31 +86,31 @@ public class DefaultParallelEdgeIndexFunction<V,E> implements EdgeIndexFunction<
     	}
     	int count=0;
     	for(E other : commonEdgeSet) {
-    		if(context.element.equals(other) == false) {
-    			edge_index.put(Context.<Graph<V,E>,E>getInstance(context.graph,other), count);
+    		if(e.equals(other) == false) {
+    			edge_index.put(Context.<Graph<V,E>,E>getInstance(graph,other), count);
     			count++;
     		}
     	}
-    	edge_index.put(context, count);
+    	edge_index.put(Context.<Graph<V,E>,E>getInstance(graph,e), count);
     	return count;
      }
     
-    protected int getIndex(Context<Graph<V,E>,E> context, V v) {
+    protected int getIndex(Graph<V,E> graph, E e, V v) {
     	Collection<E> commonEdgeSet = new HashSet<E>();
-    	for(E another : context.graph.getIncidentEdges(v)) {
-    		V u = context.graph.getOpposite(v, another);
+    	for(E another : graph.getIncidentEdges(v)) {
+    		V u = graph.getOpposite(v, another);
     		if(u.equals(v)) {
     			commonEdgeSet.add(another);
     		}
     	}
     	int count=0;
     	for(E other : commonEdgeSet) {
-    		if(context.element.equals(other) == false) {
-    			edge_index.put(Context.<Graph<V,E>,E>getInstance(context.graph,other), count);
+    		if(e.equals(other) == false) {
+    			edge_index.put(Context.<Graph<V,E>,E>getInstance(graph,other), count);
     			count++;
     		}
     	}
-    	edge_index.put(context, count);
+    	edge_index.put(Context.<Graph<V,E>,E>getInstance(graph,e), count);
     	return count;
     }
 
@@ -123,8 +123,8 @@ public class DefaultParallelEdgeIndexFunction<V,E> implements EdgeIndexFunction<
      */
     public void reset(Graph<V,E> graph, E e) {
     	Pair<V> endpoints = graph.getEndpoints(e);
-        getIndex(Context.<Graph<V,E>,E>getInstance(graph,e), endpoints.getFirst());
-        getIndex(Context.<Graph<V,E>,E>getInstance(graph,e), endpoints.getFirst(), endpoints.getSecond());
+        getIndex(graph, e, endpoints.getFirst());
+        getIndex(graph, e, endpoints.getFirst(), endpoints.getSecond());
     }
     
     /**
