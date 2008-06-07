7eba98d3766dbd4f8af0155f8773013537c2260e
e-flat <e-flat>
2008-06-07 22:04:15 +0000

    refactored; added new constructors and parameters

--- jung-algorithms/src/main/java/edu/uci/ics/jung/algorithms/scoring/DistanceCentralityScorer.java
+++ jung-algorithms/src/main/java/edu/uci/ics/jung/algorithms/scoring/DistanceCentralityScorer.java
@@ -23,9 +23,10 @@ import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.Hypergraph;
 
 /**
- * Assigns scores to vertices based on their distances to each other vertex in the graph.
+ * Assigns scores to vertices based on their distances to each other vertex 
+ * in the graph.
  * 
- * NOTE: This class optionally normalizes its results depending on the value of its
+ * NOTE: This class optionally normalizes its results based on the value of its
  * 'averaging' constructor parameter.  If it is <code>true</code>, then the value returned for vertex v is the
  * _average_ distance from v to all other vertices; this is usually called <i>closeness centrality</i>.
  * If it is <code>false</code>, then the value returned is the _total_ distance from
@@ -52,72 +53,162 @@ public class DistanceCentralityScorer<V,E> implements VertexScorer<V, Double>
     protected Map<V, Double> output;
     
     /**
-     * Specifies whether the values returned are the sum of the v-distances or the mean v-distance.
+     * Specifies whether the values returned are the sum of the v-distances
+     * or the mean v-distance.
      */
     protected boolean averaging;
     
     /**
-     * Specifies whether the values returned should ignore missing distance values, or throw an exception.
-     * (Throws an exception by default.)
+     * Specifies whether, for a vertex <code>v</code> with missing (null) distances, 
+     * <code>v</code>'s score should ignore the missing values or be set to 'null'.
+     * Defaults to 'true'.
      */
-    protected boolean ignore_nulls;
+    protected boolean ignore_missing;
+
+    /**
+     * Specifies whether the values returned should ignore self-distances 
+     * (distances from <code>v</code> to itself).
+     * Defaults to 'true'.
+     */
+    protected boolean ignore_self_distances;
     
     /**
-     * Creates an instance with the specified graph, distance metric, and averaging behavior.
+     * Creates an instance with the specified graph, distance metric, and 
+     * averaging behavior.
      * 
      * @param graph     The graph on which the vertex scores are to be calculated.
-     * @param distance  The metric to use for specifying the distance between pairs of vertices.
-     * @param averaging Specifies whether the values returned is the sum of all v-distances or the mean v-distance.
+     * @param distance  The metric to use for specifying the distance between 
+     * pairs of vertices.
+     * @param averaging Specifies whether the values returned is the sum of all 
+     * v-distances or the mean v-distance.
+     * @param ignore_missing	Specifies whether scores for missing distances 
+     * are to ignore missing distances or be set to null.
+     * @param ignore_self_distances	Specifies whether distances from a vertex
+     * to itself should be included in its score.
      */
-    public DistanceCentralityScorer(Hypergraph<V,E> graph, Distance<V> distance, boolean averaging)
+    public DistanceCentralityScorer(Hypergraph<V,E> graph, Distance<V> distance, 
+    		boolean averaging, boolean ignore_missing, 
+    		boolean ignore_self_distances)
     {
         this.graph = graph;
         this.distance = distance;
         this.averaging = averaging;
+        this.ignore_missing = ignore_missing;
+        this.ignore_self_distances = ignore_self_distances;
+    }
+
+    /**
+     * Equivalent to <code>this(graph, distance, averaging, true, true)</code>.
+     * 
+     * @param graph     The graph on which the vertex scores are to be calculated.
+     * @param distance  The metric to use for specifying the distance between 
+     * pairs of vertices.
+     * @param averaging Specifies whether the values returned is the sum of all 
+     * v-distances or the mean v-distance.
+     */
+    public DistanceCentralityScorer(Hypergraph<V,E> graph, Distance<V> distance, 
+    		boolean averaging)
+    {
+    	this(graph, distance, averaging, true, true);
     }
     
     /**
-     * Creates an instance with the specified graph and averaging behavior.  Internally creates a <code>Distance</code>
-     * instance based on the specified edge weights.
+     * Creates an instance with the specified graph and averaging behavior
+     * whose vertex distances are calculated based on the specified edge
+     * weights.  
      * 
-     * @param graph         The graph on which the vertex scores are to be calculated.
-     * @param edge_weights  The edge weights to use for specifying the distance between pairs of vertices.
-     * @param averaging     Specifies whether the values returned is the sum of all v-distances or the mean v-distance.
+     * @param graph         The graph on which the vertex scores are to be 
+     * calculated.
+     * @param edge_weights  The edge weights to use for specifying the distance 
+     * between pairs of vertices.
+     * @param averaging     Specifies whether the values returned is the sum of 
+     * all v-distances or the mean v-distance.
+     * @param ignore_missing	Specifies whether scores for missing distances 
+     * are to ignore missing distances or be set to null.
+     * @param ignore_self_distances	Specifies whether distances from a vertex
+     * to itself should be included in its score.
+     */
+    public DistanceCentralityScorer(Hypergraph<V,E> graph, 
+            Transformer<E, ? extends Number> edge_weights, boolean averaging,
+            boolean ignore_missing, boolean ignore_self_distances)
+    {
+        this(graph, new DijkstraDistance<V,E>(graph, edge_weights), averaging,
+        	ignore_missing, ignore_self_distances);
+    }
+    
+    /**
+     * Equivalent to <code>this(graph, edge_weights, averaging, true, true)</code>.
+     * @param graph         The graph on which the vertex scores are to be 
+     * calculated.
+     * @param edge_weights  The edge weights to use for specifying the distance 
+     * between pairs of vertices.
+     * @param averaging     Specifies whether the values returned is the sum of 
+     * all v-distances or the mean v-distance.
      */
     public DistanceCentralityScorer(Hypergraph<V,E> graph, 
             Transformer<E, ? extends Number> edge_weights, boolean averaging)
     {
-        this(graph, new DijkstraDistance<V,E>(graph, edge_weights), averaging);
-        setIgnoreMissingDistances(true);
+        this(graph, new DijkstraDistance<V,E>(graph, edge_weights), averaging,
+        	true, true);
     }
     
-    public DistanceCentralityScorer(Graph<V,E> graph, boolean averaging)
+    /**
+     * Creates an instance with the specified graph and averaging behavior
+     * whose vertex distances are calculated on the unweighted graph.  
+     * 
+     * @param graph         The graph on which the vertex scores are to be 
+     * calculated.
+     * @param averaging     Specifies whether the values returned is the sum of 
+     * all v-distances or the mean v-distance.
+     * @param ignore_missing	Specifies whether scores for missing distances 
+     * are to ignore missing distances or be set to null.
+     * @param ignore_self_distances	Specifies whether distances from a vertex
+     * to itself should be included in its score.
+     */
+    public DistanceCentralityScorer(Graph<V,E> graph, boolean averaging,
+            boolean ignore_missing, boolean ignore_self_distances)
     {
-        this(graph, new UnweightedShortestPath<V,E>(graph), averaging);
-        setIgnoreMissingDistances(true);
+        this(graph, new UnweightedShortestPath<V,E>(graph), averaging, 
+        	ignore_missing, ignore_self_distances);
     }
 
+    /**
+     * Equivalent to <code>this(graph, averaging, true, true)</code>.
+     * @param graph         The graph on which the vertex scores are to be 
+     * calculated.
+     * @param averaging     Specifies whether the values returned is the sum of 
+     * all v-distances or the mean v-distance.
+     */
+    public DistanceCentralityScorer(Graph<V,E> graph, boolean averaging)
+    {
+        this(graph, new UnweightedShortestPath<V,E>(graph), averaging, true,
+        	true);
+    }
+
+    /**
+     * 
+     */
     public void evaluate()
     {
         for (V v : graph.getVertices())
         {
-            double sum = 0;
+            Double sum = 0.0;
             for (Map.Entry<V,Number> entry : distance.getDistanceMap(v).entrySet())
             {
-                double distance = entry.getValue().doubleValue();
-                sum += distance;
+            	Number w_distance = entry.getValue();
+				if (w_distance == null)
+					if (ignore_missing)
+						continue;
+					else
+						sum = null;
+				else
+					sum += w_distance.doubleValue();
             }
             if (averaging)
                 output.put(v, sum / graph.getVertexCount());
             else
                 output.put(v, sum);
         }
-        setIgnoreMissingDistances(true);
-    }
-    
-    public void setIgnoreMissingDistances(boolean ignore)
-    {
-        this.ignore_nulls = ignore;
     }
     
     public Transformer<V, Double> getVertexScores()
