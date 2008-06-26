3c6764907976913896fe78a1a364ac8c5f006a5c
e-flat <e-flat>
2008-06-26 06:13:28 +0000

    updated test

--- jung-io/src/test/java/edu/uci/ics/jung/io/TestGraphMLWriter.java
+++ jung-io/src/test/java/edu/uci/ics/jung/io/TestGraphMLWriter.java
@@ -13,32 +13,75 @@ package edu.uci.ics.jung.io;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
+import javax.xml.parsers.ParserConfigurationException;
+
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import org.apache.commons.collections15.Factory;
+import org.apache.commons.collections15.FactoryUtils;
 import org.apache.commons.collections15.Transformer;
+import org.xml.sax.SAXException;
 
+import edu.uci.ics.jung.algorithms.util.SettableTransformer;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.util.TestGraphs;
 
 public class TestGraphMLWriter extends TestCase
 {
-    public void testBasicWrite() throws IOException
+    public void testBasicWrite() throws IOException, ParserConfigurationException, SAXException
     {
         Graph<String, Number> g = TestGraphs.createTestGraph(true);
         GraphMLWriter<String, Number> gmlw = new GraphMLWriter<String, Number>();
-        gmlw.addEdgeData("weight", "integer value for the edge", -1, 
-        		new Transformer<Number, String>() 
-        		{ 
-        			public String transform(Number n) 
-        			{ 
-        				return String.valueOf(n.intValue()); 
-        			} 
-        		});
+        Transformer<Number, String> edge_weight = new Transformer<Number, String>() 
+		{ 
+			public String transform(Number n) 
+			{ 
+				return String.valueOf(n.intValue()); 
+			} 
+		};
+
+        gmlw.addEdgeData("weight", "integer value for the edge", -1, edge_weight);
+        gmlw.setEdgeIDs(edge_weight);
         gmlw.save(g, new FileWriter("src/test/resources/testbasicwrite.graphml"));
         
         // TODO: now read it back in and compare the graph connectivity 
         // and other metadata with what's in TestGraphs.pairs[], etc.
+        Factory<String> vertex_factory = null;
+        Factory<Object> edge_factory = FactoryUtils.instantiateFactory(Object.class);
+        GraphMLReader<Graph<String, Object>, String, Object> gmlr = 
+        	new GraphMLReader<Graph<String, Object>, String, Object>(
+        			vertex_factory, edge_factory);
+        Graph<String, Object> g2 = new DirectedSparseGraph<String, Object>();
+        gmlr.load("src/test/resources/testbasicwrite.graphml", g2);
+        Map<String, SettableTransformer<Object, String>> edge_data = 
+        	gmlr.getEdgeData();
+        Transformer<Object, String> edge_weight2 = edge_data.get("weight");
+        Assert.assertEquals(g2.getEdgeCount(), g.getEdgeCount());
+        List<String> g_vertices = new ArrayList<String>(g.getVertices());
+        List<String> g2_vertices = new ArrayList<String>(g2.getVertices());
+        Collections.sort(g_vertices); 
+		Collections.sort(g2_vertices);
+        Assert.assertEquals(g_vertices, g2_vertices);
+        for (String v : g2.getVertices())
+        {
+        	for (String w : g2.getVertices())
+        	{
+        		Assert.assertEquals(g.areNeighbors(v, w), 
+        				g2.areNeighbors(v, w));
+        		Number n = g.findEdge(v, w);
+        		Object o = g2.findEdge(v, w);
+        		if (n != null)
+        			Assert.assertEquals(edge_weight2.transform(o),
+        					edge_weight.transform(n));
+        	}
+        }
         
         // TODO: delete graph file when done
     }
@@ -57,7 +100,8 @@ public class TestGraphMLWriter extends TestCase
         		});
         gmlw.save(g, new FileWriter("src/test/resources/testmixedgraph.graphml"));
         
-        // TODO: now read it back in and compare the graph connectivity and edge weights with what's in TestGraphs.pairs[]
+        // TODO: now read it back in and compare the graph connectivity 
+        // and other metadata with what's in TestGraphs, etc.
         
         // TODO: delete graph file when done
     }
