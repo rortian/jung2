dd9cb312842e0db2d6a5a1e10d3b456fce18d357
e-flat <e-flat>
2008-07-15 05:28:16 +0000

    changed edge weight type changed score type (new HITS.Score) refactored normalization into its own method changed semantics: now uses constant edge weight by default, normalizes according to sum of squared scores

--- jung-algorithms/src/main/java/edu/uci/ics/jung/algorithms/scoring/HITS.java
+++ jung-algorithms/src/main/java/edu/uci/ics/jung/algorithms/scoring/HITS.java
@@ -14,25 +14,52 @@ package edu.uci.ics.jung.algorithms.scoring;
 import org.apache.commons.collections15.Transformer;
 
 import edu.uci.ics.jung.algorithms.scoring.util.ScoringUtils;
+import edu.uci.ics.jung.graph.DirectedGraph;
 import edu.uci.ics.jung.graph.Graph;
-import edu.uci.ics.jung.graph.util.Pair;
+import edu.uci.ics.jung.graph.UndirectedGraph;
 
 public class HITS<V,E> extends HITSWithPriors<V,E>
 {
 
-    public HITS(Graph<V,E> g, Transformer<E, Pair<Number>> edge_weights, double alpha)
+    public HITS(Graph<V,E> g, Transformer<E, Double> edge_weights, double alpha)
     {
-        super(g, edge_weights, ScoringUtils.getTwoValueUniformRootPrior(g.getVertices()), alpha);
+        super(g, edge_weights, ScoringUtils.getHITSUniformRootPrior(g.getVertices()), alpha);
     }
 
-//    public HITS(Graph<V,E> g, double alpha)
-//    {
-//        super(g, ScoringUtils.getTwoValueUniformRootPrior(g.getVertices()), alpha);
-//    }
+    public HITS(DirectedGraph<V,E> g, double alpha)
+    {
+        super(g, ScoringUtils.getHITSUniformRootPrior(g.getVertices()), alpha);
+    }
 
-//    public HITS(Graph<V,E> g)
-//    {
-//        this(g, 0.0);
-//    }
+    public HITS(UndirectedGraph<V,E> g, double alpha)
+    {
+        super(g, ScoringUtils.getHITSUniformRootPrior(g.getVertices()), alpha);
+    }
+    
+    public HITS(DirectedGraph<V,E> g)
+    {
+        this(g, 0.0);
+    }
 
+    public HITS(UndirectedGraph<V,E> g)
+    {
+        this(g, 0.0);
+    }
+
+    public static class Scores
+    {
+    	public double hub;
+    	public double authority;
+    	
+    	public Scores(double hub, double authority)
+    	{
+    		this.hub = hub;
+    		this.authority = authority;
+    	}
+    	
+    	public String toString()
+    	{
+    		return String.format("[h:%.4f,a:%.4f]", this.hub, this.authority);
+    	}
+    }
 }
--- jung-algorithms/src/main/java/edu/uci/ics/jung/algorithms/scoring/HITSWithPriors.java
+++ jung-algorithms/src/main/java/edu/uci/ics/jung/algorithms/scoring/HITSWithPriors.java
@@ -12,43 +12,43 @@
 package edu.uci.ics.jung.algorithms.scoring;
 
 import org.apache.commons.collections15.Transformer;
+import org.apache.commons.collections15.functors.ConstantTransformer;
 
-import edu.uci.ics.jung.algorithms.scoring.util.UniformInOut;
-import edu.uci.ics.jung.graph.DirectedGraph;
 import edu.uci.ics.jung.graph.Graph;
-import edu.uci.ics.jung.graph.UndirectedGraph;
-import edu.uci.ics.jung.graph.util.Pair;
 
-public class HITSWithPriors<V, E> extends AbstractIterativeScorerWithPriors<V, E, Pair<Number>, Pair<Number>>
+public class HITSWithPriors<V, E> 
+	extends AbstractIterativeScorerWithPriors<V,E,HITS.Scores,Double>
 {
     protected double disappearing_hub;
     protected double disappearing_auth;
 
     public HITSWithPriors(Graph<V,E> g,
-            Transformer<E, Pair<Number>> edge_weights,
-            Transformer<V, Pair<Number>> vertex_priors, double alpha)
+            Transformer<E, Double> edge_weights,
+            Transformer<V, HITS.Scores> vertex_priors, double alpha)
     {
         super(g, edge_weights, vertex_priors, alpha);
     }
 
-    public HITSWithPriors(DirectedGraph<V,E> g,
-            Transformer<V, Pair<Number>> vertex_priors, double alpha)
+    public HITSWithPriors(Graph<V,E> g, 
+          Transformer<V, HITS.Scores> vertex_priors, double alpha)
     {
-        this(g, new UniformInOut<V,E>(g), vertex_priors, alpha);
+    	super(g, new ConstantTransformer(1.0), vertex_priors, alpha);
     }
 
-    public HITSWithPriors(UndirectedGraph<V,E> g, 
-            Transformer<V, Pair<Number>> vertex_priors, double alpha)
-    {
-        this(g, new UniformInOut<V,E>(g), vertex_priors, alpha);
-    }
     
-//    public HITSWithPriors(Graph<V, E> g,
-//            Transformer<V, Pair<Number>> vertex_priors, double alpha)
+//    public HITSWithPriors(DirectedGraph<V,E> g,
+//            Transformer<V, Pair<Double>> vertex_priors, double alpha)
+//    {
+//    	super(g, vertex_priors, alpha);
+//    	this.edge_weights = new UniformInOut<V,E>(g);
+//    }
+//
+//    public HITSWithPriors(UndirectedGraph<V,E> g, 
+//            Transformer<V, Pair<Double>> vertex_priors, double alpha)
 //    {
 //        super(g, vertex_priors, alpha);
+//        this.edge_weights = new UniformIncidentPair<V,E>(g);
 //    }
-
     
     @Override
     public double update(V v)
@@ -59,20 +59,20 @@ public class HITSWithPriors<V, E> extends AbstractIterativeScorerWithPriors<V, E
         for (E e : graph.getInEdges(v))
         {
             V w = graph.getOpposite(v, e);
-            auth += (getHubScore(w) * getEdgeWeight(w, e).getSecond().doubleValue());
+            auth += (getHubScore(w) * getEdgeWeight(w, e)); //.getSecond().doubleValue());
         }
         
         double hub = 0;
         for (E e : graph.getOutEdges(v))
         {
             V x = graph.getOpposite(v,e);
-            hub += (getAuthScore(x) * getEdgeWeight(x, e).getFirst().doubleValue()); 
+            hub += (getAuthScore(x) * getEdgeWeight(x, e)); // .getFirst().doubleValue()); 
         }
         
         // modify total_input according to alpha
         auth = auth * (1 - alpha) + getAuthPrior(v) * alpha;
         hub = hub * (1 - alpha) + getHubPrior(v) * alpha;
-        setOutputValue(v, new Pair<Number>(hub, auth));
+        setOutputValue(v, new HITS.Scores(hub, auth));
 
         return Math.max(Math.abs(getHubScore(v) - hub), Math.abs(getAuthScore(v) - auth));
     }
@@ -80,21 +80,53 @@ public class HITSWithPriors<V, E> extends AbstractIterativeScorerWithPriors<V, E
     @Override
     protected void afterStep()
     {
-        if (disappearing_hub > 0 || disappearing_auth > 0)
-        {
-            for (V v : graph.getVertices())
-            {
-                double new_hub = getOutputValue(v).getFirst().doubleValue() + 
-                    (1 - alpha) * (disappearing_hub * getHubPrior(v));
-                double new_auth = getOutputValue(v).getSecond().doubleValue() + 
-                    (1 - alpha) * (disappearing_hub * getAuthPrior(v));
-                setOutputValue(v, new Pair<Number>(new_hub, new_auth));
-            }
-            disappearing_hub = 0;
-            disappearing_auth = 0;
-        }
+//        if (disappearing_hub > 0 || disappearing_auth > 0)
+//        {
+//            for (V v : graph.getVertices())
+//            {
+//                double new_hub = getOutputValue(v).getFirst().doubleValue() + 
+//                    (1 - alpha) * (disappearing_hub * getHubPrior(v));
+//                double new_auth = getOutputValue(v).getSecond().doubleValue() + 
+//                    (1 - alpha) * (disappearing_hub * getAuthPrior(v));
+//                setOutputValue(v, new Pair<Double>(new_hub, new_auth));
+//            }
+//            disappearing_hub = 0;
+//            disappearing_auth = 0;
+//        }
+    	disappearing_hub = 0;
+    	disappearing_auth = 0; 
+        
+    	normalizeScores();
+
+    	
         super.afterStep();
     }
+
+	/**
+	 * Normalizes scores so that sum of their squares = 1.
+	 */
+	protected void normalizeScores() {
+    	double hub_ssum = 0;
+    	double auth_ssum = 0;
+    	for (V v : graph.getVertices())
+    	{
+    		double hub_val = getOutputValue(v).hub;
+    		double auth_val = getOutputValue(v).authority;
+    		hub_ssum += (hub_val * hub_val);
+    		auth_ssum += (auth_val * auth_val);
+    	}
+
+    	hub_ssum = Math.sqrt(hub_ssum);
+    	auth_ssum = Math.sqrt(auth_ssum);
+    	
+    	for (V v : graph.getVertices())
+    	{
+    		HITS.Scores values = getOutputValue(v);
+    		setOutputValue(v, new HITS.Scores(
+    				values.hub / hub_ssum,
+    				values.authority / auth_ssum));
+    	}
+	}
     
     @Override
     protected void collectDisappearingPotential(V v)
@@ -118,21 +150,22 @@ public class HITSWithPriors<V, E> extends AbstractIterativeScorerWithPriors<V, E
     
     public double getHubScore(V v)
     {
-        return getCurrentValue(v).getFirst().doubleValue();
+        return getCurrentValue(v).hub;
     }
     
     public double getAuthScore(V v)
     {
-        return getCurrentValue(v).getSecond().doubleValue();
+        return getCurrentValue(v).authority;
     }
 
     protected double getHubPrior(V v)
     {
-        return getVertexPrior(v).getFirst().doubleValue();
+        return getVertexPrior(v).hub;
     }
     
     protected double getAuthPrior(V v)
     {
-        return getVertexPrior(v).getSecond().doubleValue();
+        return getVertexPrior(v).authority;
     }
+    
 }
