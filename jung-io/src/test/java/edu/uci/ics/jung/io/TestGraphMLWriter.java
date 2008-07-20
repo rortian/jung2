68655ea72a33eee34e9454a02e244b6bc9cd2e1b
e-flat <e-flat>
2008-07-20 22:33:01 +0000

    refined tests to check more about topology, values now properly deletes generated files on completion

--- jung-io/src/test/java/edu/uci/ics/jung/io/TestGraphMLWriter.java
+++ jung-io/src/test/java/edu/uci/ics/jung/io/TestGraphMLWriter.java
@@ -11,9 +11,11 @@
  */
 package edu.uci.ics.jung.io;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -21,6 +23,7 @@ import java.util.Map;
 import javax.xml.parsers.ParserConfigurationException;
 
 import junit.framework.Assert;
+import junit.framework.ComparisonFailure;
 import junit.framework.TestCase;
 
 import org.apache.commons.collections15.Transformer;
@@ -29,6 +32,7 @@ import org.xml.sax.SAXException;
 
 import edu.uci.ics.jung.graph.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.SparseMultigraph;
 import edu.uci.ics.jung.graph.util.TestGraphs;
 
 public class TestGraphMLWriter extends TestCase
@@ -69,47 +73,99 @@ public class TestGraphMLWriter extends TestCase
         	gmlr.getEdgeMetadata();
         Transformer<Object, String> edge_weight2 = 
         	(Transformer<Object, String>)edge_metadata.get("weight").transformer;
-        Assert.assertEquals(g2.getEdgeCount(), g.getEdgeCount());
-        List<String> g_vertices = new ArrayList<String>(g.getVertices());
-        List<String> g2_vertices = new ArrayList<String>(g2.getVertices());
-        Collections.sort(g_vertices); 
-		Collections.sort(g2_vertices);
-        Assert.assertEquals(g_vertices, g2_vertices);
-        for (String v : g2.getVertices())
-        {
-        	for (String w : g2.getVertices())
-        	{
-        		Assert.assertEquals(g.isNeighbor(v, w), 
-        				g2.isNeighbor(v, w));
-        		Number n = g.findEdge(v, w);
-        		Object o = g2.findEdge(v, w);
-        		if (n != null)
-        			Assert.assertEquals(edge_weight2.transform(o),
-        					edge_weight.transform(n));
-        	}
-        }
+        validateTopology(g, g2, edge_weight, edge_weight2);
         
         // TODO: delete graph file when done
+        File f = new File("src/test/resources/testbasicwrite.graphml");
+        f.delete();
     }
     
-    public void testMixedGraph() throws IOException
+    public void testMixedGraph() throws IOException, ParserConfigurationException, SAXException
     {
-        Graph<Integer, Number> g = TestGraphs.getSmallGraph();
-        GraphMLWriter<Integer, Number> gmlw = new GraphMLWriter<Integer, Number>();
-        gmlw.addEdgeData("weight", null, null, 
-        		new Transformer<Number, String>() 
-        		{ 
-        			public String transform(Number n) 
-        			{ 
-        				return String.valueOf(n.intValue()); 
-        			} 
-        		});
+        Graph<String, Number> g = TestGraphs.getSmallGraph();
+        GraphMLWriter<String, Number> gmlw = new GraphMLWriter<String, Number>();
+        Transformer<Number, String> edge_weight = new Transformer<Number, String>() 
+        { 
+            public String transform(Number n) 
+            { 
+                return String.valueOf(n.doubleValue()); 
+            } 
+        };
+
+        gmlw.addEdgeData("weight", "integer value for the edge", 
+                Integer.toString(-1), edge_weight);
+        gmlw.setEdgeIDs(edge_weight);
         gmlw.save(g, new FileWriter("src/test/resources/testmixedgraph.graphml"));
-        
+
         // TODO: now read it back in and compare the graph connectivity 
         // and other metadata with what's in TestGraphs, etc.
+        GraphMLReader<Graph<String,Object>,String,Object> gmlr = 
+            new GraphMLReader<Graph<String,Object>,String,Object>();
+        Graph<String,Object> g2 = new SparseMultigraph<String,Object>();
+        gmlr.load("src/test/resources/testmixedgraph.graphml", g2);
+        Map<String, GraphMLMetadata<Object>> edge_metadata = 
+            gmlr.getEdgeMetadata();
+        Transformer<Object, String> edge_weight2 = 
+            (Transformer<Object, String>)edge_metadata.get("weight").transformer;
+        validateTopology(g, g2, edge_weight, edge_weight2);
         
         // TODO: delete graph file when done
+        File f = new File("src/test/resources/testmixedgraph.graphml");
+        f.delete();
     }
 
+    public <T extends Comparable<T>> void validateTopology(Graph<T,Number> g, Graph<T,Object> g2,
+            Transformer<Number,String> edge_weight, Transformer<Object,String> edge_weight2)
+    {
+        Assert.assertEquals(g2.getEdgeCount(), g.getEdgeCount());
+        List<T> g_vertices = new ArrayList<T>(g.getVertices());
+        List<T> g2_vertices = new ArrayList<T>(g2.getVertices());
+        Collections.sort(g_vertices); 
+        Collections.sort(g2_vertices);
+        Assert.assertEquals(g_vertices, g2_vertices);
+
+        List g_edges = new ArrayList();
+        for (Number n : g.getEdges())
+            g_edges.add(String.valueOf(n));
+        List g2_edges = new ArrayList(g2.getEdges());
+        Collections.sort(g_edges); 
+        Collections.sort(g2_edges);
+        Assert.assertEquals(g_edges, g2_edges);
+        
+        for (T v : g2.getVertices())
+        {
+            for (T w : g2.getVertices())
+            {
+                Assert.assertEquals(g.isNeighbor(v, w), 
+                        g2.isNeighbor(v, w));
+                List e = new ArrayList();
+                for (Number n : g.findEdgeSet(v, w))
+                    e.add(String.valueOf(n));
+                List e2 = new ArrayList(g2.findEdgeSet(v, w));
+                Assert.assertEquals(e.size(), e2.size());
+                Collections.sort(e);
+                Collections.sort(e2);
+                Assert.assertEquals(e, e2);
+            }
+        }
+        
+        for (Object o : g2.getEdges())
+        {
+            String weight = edge_weight.transform(new Double((String)o));
+            String weight2 = edge_weight2.transform(o);
+            Assert.assertEquals(weight2, weight);
+        }        
+//                Number n = g.findEdge(v, w);
+//                Object o = g2.findEdge(v, w);
+//                if (n != null)
+//                {
+//                    String weight = edge_weight.transform(n);
+//                    String weight2 = edge_weight2.transform(o);
+//                    Assert.assertEquals(weight2, weight);
+//                }
+//            }
+//        }
+        
+    }
+    
 }
