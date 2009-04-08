f363e1308247cb56116c6d51ba59f53cfe1ff39b
tomnelson <tomnelson>
2009-04-08 08:49:56 +0000

    fixed license

--- jung-3d-demos/src/main/java/edu/uci/ics/jung/samples/GraphDemo.java
+++ jung-3d-demos/src/main/java/edu/uci/ics/jung/samples/GraphDemo.java
@@ -1,17 +1,11 @@
 /*
+ * Copyright (c) 2005, the JUNG Project and the Regents of the University of
+ * California All rights reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * This software is open-source under the BSD license; see either "license.txt"
+ * or http://jung.sourceforge.net/license.txt for a description.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+ * 
  */
 package edu.uci.ics.jung.samples;
 
